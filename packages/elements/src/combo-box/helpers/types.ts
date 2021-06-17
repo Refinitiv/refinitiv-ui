be111cdab89911774f85861c99e751761d244ca7
Wasuwat Limsuparhat <wasuwat.limsuparhat@thomsonreuters.com>
2021-06-17 15:35:10 +0700

    fix(combo-box): provide default value to generic `ComboBoxData` type

--- packages/elements/src/combo-box/helpers/types.ts
+++ packages/elements/src/combo-box/helpers/types.ts
@@ -1,14 +1,13 @@
 import { DataItem } from '@refinitiv-ui/utils';
+import { ItemData } from '../../item';
 
 /**
  * Predicate callback
- *
  * Matches item against filter function
  *
  * @param item Item to filter
  * @return Does item match filter
  */
-export type ComboBoxFilter<T extends DataItem> = (item: T) => boolean;
+export type ComboBoxFilter<T extends DataItem = ItemData> = (item: T) => boolean;
 
-
-export type ComboBoxData<T> = T[] | Promise<T[]>;
+export type ComboBoxData<T extends DataItem = ItemData> = T[] | Promise<T[]>;
