428cf91fdc70a28042bd8bb3cdf08d8538db7b05
Sakchai Homhual <sakchai.homhual@thomsonreuters.com>
2021-06-28 19:11:10 +0700

    fix: list linting

--- packages/elements/src/list/extensible-function.ts
+++ packages/elements/src/list/extensible-function.ts
@@ -4,8 +4,10 @@
  * ! Do not import this module !
  */
 export abstract class ExtensibleFunction extends Function {
+  // eslint-disable-next-line @typescript-eslint/ban-types
   constructor (fn: Function) {
     super();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return Object.setPrototypeOf(fn, new.target.prototype);
   }
 }
