58d2a12fbdef6da0f9c5694053af8615a364eba1
Songkrod Thanavorn <songkrod.thanavorn@refinitiv.com>
2021-07-08 15:47:10 +0700

    chore: add a populate version script to release script

--- scripts/release/index.js
+++ scripts/release/index.js
@@ -5,7 +5,8 @@ const childProcess = require('child_process');
 const scripts = [
   path.resolve(__dirname, './api-analyzer.js'),
   path.resolve(__dirname, './jsxdts-generator.js'),
-  path.resolve(__dirname, './theme-extractor.js')
+  path.resolve(__dirname, './theme-extractor.js'),
+  path.resolve(__dirname, './versioning.js')
 ];
 
 scripts.forEach(script => childProcess.fork(script));
