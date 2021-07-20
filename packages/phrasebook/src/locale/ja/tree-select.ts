877105df2cf26638ece525fc5797b4c2f2f6d485
Songkrod Thanavorn <songkrod.thanavorn@refinitiv.com>
2021-07-20 16:07:29 +0700

    chore(phrasebook): change search result wording of tree-select

--- packages/phrasebook/src/locale/ja/tree-select.ts
+++ packages/phrasebook/src/locale/ja/tree-select.ts
@@ -7,15 +7,15 @@ const translations = {
   ...comboboxTranslations,
 
   // used as a toggle control
-  FULL_LIST: '一覧を表示',
-  SELECTED: '選択項目のみ表示',
+  FULL_LIST: 'すべて',
+  SELECTED: '選択項目のみ',
   EXPAND_COLLAPSE: '{expansion, select, false {すべてを展開} true {すべてを折りたたむ}}',
   SELECT_CONTROL: '{selected, select, false {すべて選択} true {選択をすべて解除}}',
   // button control
   DONE: '完了',
   // selection/filter feedback
   SELECTED_NUM: '{numSelected, plural, =0 {} =1 {1 件選択済み} other {# 件選択済み}}',
-  MATCHES_NUM: '{numMatched, plural, =0 {該当なし} =1 {1件見つかりました} other {# 件見つかりました}}'
+  MATCHES_NUM: '{numMatched, plural, =0 {該当項目はありません} =1 {検索結果 – 1件} other {検索結果 – #件}}'
 };
 
 Phrasebook.define('ja', 'ef-tree-select', translations);
