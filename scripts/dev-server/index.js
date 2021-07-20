b7855e409d10d9c8b9f31a34953470549295a8ab
Aleksejs Goremikins <aleksejs.goremikins@refinitiv.com>
2021-07-20 11:25:19 +0100

    feat: use latest FormatJS polyfills and ICU parser refactor: change ICU syntax to comply with icu4j specification chore: deprecate transformCommonJS plugin as it is not required with latest formatjs polyfills

--- scripts/dev-server/index.js
+++ scripts/dev-server/index.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 const middlewareOverrideDemoPath = require('./middleware/override-demo-path');
-const pluginTransformCommonJS = require('./plugins/transform-commonjs');
 const server = require('@web/dev-server');
 
 module.exports = {
   server,
-  pluginTransformCommonJS,
   middlewareOverrideDemoPath
 };
