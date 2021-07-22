be6b50df285720742bd7eac8948bbb3fb763b3d3
Aleksejs Goremikins <aleksejs.goremikins@refinitiv.com>
2021-07-22 11:59:28 +0100

    chore: add npm and node version

--- karma.config.js
+++ karma.config.js
@@ -97,7 +97,7 @@ const baseConfig = {
   autoWatch: argv.watch,
   singleRun: !argv.watch,
   basePath: ROOT, // must be in the root in order for node_modules to be resolved correctly
-  concurrency: Infinity, // Set the value to `1`, When Karma has a problem to connect a test browser on Windows.
+  concurrency: 1, // Set the value to `1`, When Karma has a problem to connect a test browser on Windows.
   browserNoActivityTimeout: 60000,
   browserDisconnectTimeout: 60000,
   files,
