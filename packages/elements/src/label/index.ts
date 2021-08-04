2f9d7826aedc43fbc6eb20f69c93c0e5bd30d221
Trem <tremayne.christ@refinitiv.com>
2021-08-04 18:31:52 +0100

    feat(label): add IE support

--- packages/elements/src/label/index.ts
+++ packages/elements/src/label/index.ts
@@ -26,6 +26,17 @@ const observerOptions = {
 const _ = ' ';
 
 /**
+ * Helper to check if the browser is IE
+ * @returns True if the browser is IE
+ */
+const isIE = () => !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g);
+
+/**
+ * Determines if the browser is legacy or modern.
+ */
+const browserType = isIE() ? 'legacy' : 'modern';
+
+/**
  * Displays a text with alternative truncation
  */
 @customElement('ef-label', {
@@ -60,24 +71,38 @@ export class Label extends BasicElement {
         flex: 0 1 auto;
         min-width: 0;
         overflow: hidden;
+        white-space: nowrap;
       }
-      .left {
-        border-left: 1px solid transparent;
-        margin-left: -1px;
+      .left.modern {
         word-break: break-all;
+        white-space: normal;
+      }
+      .left.legacy {
+        text-overflow: ellipsis;
       }
       .center {
         flex: 0 100 auto;
       }
-      .right {
+      .right.modern {
         direction: rtl;
-        white-space: nowrap;
         text-overflow: ellipsis;
+      }
+      .right.legacy span {
+        float: right;
       }
       .clamp {
         overflow: hidden;
         -webkit-box-orient: vertical;
         display: -webkit-inline-box;
+        text-overflow: ellipsis;
+        position: relative;
+      }
+      .clamp.legacy {
+        border-bottom: 8px solid transparent;
+        background: linear-gradient(currentColor, currentColor), linear-gradient(currentColor, currentColor), linear-gradient(currentColor, currentColor);
+        background-size: 2px 2px;
+        background-repeat: no-repeat;
+        background-position: 0 calc(100% + 6px), 3px calc(100% + 6px), 6px calc(100% + 6px);
       }
     `;
   }
@@ -108,7 +133,7 @@ export class Label extends BasicElement {
   /**
    * Mutation Observer used to detect changes in the Light DOM
    */
-  private mutationObserver = new MutationObserver(() => this.handleMutation());
+  private mutationObserver = new MutationObserver(() => this.recalculate(true));
 
   /**
    * Render used to display the tooltip
@@ -130,7 +155,7 @@ export class Label extends BasicElement {
     super.connectedCallback();
     addTooltipCondition(this.tooltipCondition, this.tooltipRenderer);
     this.mutationObserver.observe(this, observerOptions);
-    this.handleMutation();
+    !isIE() && this.recalculate(); // In IE the mutation will trigger
   }
 
   /**
@@ -140,7 +165,6 @@ export class Label extends BasicElement {
     super.disconnectedCallback();
     removeTooltipCondition(this.tooltipCondition);
     this.mutationObserver.disconnect();
-    this.handleMutation();
   }
 
   /**
@@ -150,18 +174,20 @@ export class Label extends BasicElement {
    */
   protected shouldShowTooltip (tooltipTarget: HTMLElement): boolean {
     const targetMatches = tooltipTarget === this;
-    const span = this.renderRoot.firstElementChild;
-    if (targetMatches && span) {
-      return span.clientHeight !== span.scrollHeight;
+    const part = this.renderRoot.firstElementChild;
+    if (targetMatches && part) {
+      return part.clientHeight !== part.scrollHeight || part.clientWidth !== part.scrollWidth;
     }
     return false;
   }
 
   /**
    * Handles any modifications to the internal HTML
+   * @param [mutation=false] is the request from a mutation event?
    * @returns {void}
    */
-  protected handleMutation (): void {
+  protected recalculate (mutation = false): void {
+    mutation; // keeping here for future use
     const oldValue = this.text;
     const raw = this.textContent || '';
     this.chunks = raw.split(_).map(chunk => chunk.trim()).filter(chunk => chunk);
@@ -190,11 +216,11 @@ export class Label extends BasicElement {
       (i < half ? left : right).push(words[i]);
     }
     const leftPart = html`
-      <div class="split left">${left.join(_)}</div>
+      <div class="split left ${browserType}">${left.join(_)}</div>
     `;
     const rightPart = right.length ? html`
       <div class="split center">&nbsp;</div>
-      <div class="split right"><span dir="ltr">${right.join(_)}</span></div>
+      <div class="split right ${browserType}"><span dir="ltr">${right.join(_)}</span></div>
     ` : undefined;
     return html`${leftPart}${rightPart}`;
   }
@@ -203,8 +229,14 @@ export class Label extends BasicElement {
    * Template for when line clamp is above 1
    */
   protected get clampTemplate (): TemplateResult {
+    let maxHeight = 'none';
+    if (browserType === 'legacy') {
+      const cs = getComputedStyle(this);
+      const lineHeight = parseFloat(cs.lineHeight) || 1.2/* css default */;
+      maxHeight = `calc(1em * ${lineHeight} * ${this.lineClamp})`;
+    }
     return html`
-      <span class="clamp" style="line-clamp:${this.lineClamp};-webkit-line-clamp:${this.lineClamp}">${this.text}</span>
+      <span class="clamp ${browserType}" style="line-clamp:${this.lineClamp};-webkit-line-clamp:${this.lineClamp};max-height:${maxHeight}">${this.text}</span>
     `;
   }
 
