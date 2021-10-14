181c12108fac1efa5eb2bf8a59d18e8271e6d1a9
AG <81616437+goremikins@users.noreply.github.com>
2021-10-14 13:38:12 +0100

    chore: make sure that version is populated for all relevant packages

--- packages/elements/scripts/release/index.js
+++ packages/elements/scripts/release/index.js
@@ -3,9 +3,17 @@ const { fork } = require('child_process');
 const path = require('path');
 const { ROOT, ELEMENTS_ROOT } = require('../helpers');
 
-fork(path.resolve(ROOT, 'scripts', 'release', 'index.js'), {
+const RELEASE_PATH = path.resolve(ROOT, 'scripts', 'release');
+
+const scripts = [
+  path.resolve(RELEASE_PATH, 'api-analyzer.js'),
+  path.resolve(RELEASE_PATH, 'jsxdts-generator.js'),
+  path.resolve(RELEASE_PATH, 'theme-extractor.js')
+];
+
+scripts.forEach(script => fork(script, {
   stdio: 'inherit',
   env: {
     PACKAGE_ROOT: ELEMENTS_ROOT
   }
-});
+}));
