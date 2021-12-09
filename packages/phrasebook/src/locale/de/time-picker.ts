958791a8698d5b234e532adb90e9bf3a2e13195d
AG <81616437+goremikins@users.noreply.github.com>
2021-12-09 09:43:51 +0000

    feat(phrasebook): change time-picker wording

--- packages/phrasebook/src/locale/de/time-picker.ts
+++ packages/phrasebook/src/locale/de/time-picker.ts
@@ -7,7 +7,7 @@ const translations = {
   SELECT_HOURS: '{value, select, null {Select hours} other {{value, plural, =1 {# hour} other {# hours}}}}',
   SELECT_MINUTES: '{value, select, null {Select minutes} other {{value, plural, =1 {# minute} other {# minutes}}}}',
   SELECT_SECONDS: '{value, select, null {Select seconds} other {{value, plural, =1 {# second} other {# seconds}}}}',
-  SELECTED: '{value, select, null {Choose Time} other {Selected time is: {showSeconds, select, true {{amPm, select, true {{value, time, ::hmsa}} other {{value, time, ::Hms}}}} other {{amPm, select, true {{value, time, ::hma}} other {{value, time, ::Hm}}}}}}}'
+  SELECTED: '{value, select, null {Selected none. Choose time} other {Selected time is: {showSeconds, select, true {{amPm, select, true {{value, time, ::hmsa}} other {{value, time, ::Hms}}}} other {{amPm, select, true {{value, time, ::hma}} other {{value, time, ::Hm}}}}}}}'
 };
 
 Phrasebook.define('de', 'ef-time-picker', translations);
