06de0232e46cffa945e3021e325218ea8fe1c011
Sarin Udompanish <sarin.udompanish@refinitiv.com>
2022-01-05 16:39:15 +0700

    chore: change import path

--- samples/react/src/App.js
+++ samples/react/src/App.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-import '@refinitiv-ui/elements/button';
-import '@refinitiv-ui/elements/toggle';
-import '@refinitiv-ui/elements/header';
-import '@refinitiv-ui/elements/interactive-chart';
-import '@refinitiv-ui/elements/dialog';
-import '@refinitiv-ui/elements/text-field';
-import '@refinitiv-ui/elements/email-field';
-import '@refinitiv-ui/elements/radio-button';
-import '@refinitiv-ui/elements/checkbox';
-import '@refinitiv-ui/elements/datetime-picker';
-import '@refinitiv-ui/elements/overlay';
-import '@refinitiv-ui/elements/overlay-menu';
-import '@refinitiv-ui/elements/tab-bar';
-import '@refinitiv-ui/elements/tab';
+import '@refinitiv-ui/elements/lib/button';
+import '@refinitiv-ui/elements/lib/toggle';
+import '@refinitiv-ui/elements/lib/header';
+import '@refinitiv-ui/elements/lib/interactive-chart';
+import '@refinitiv-ui/elements/lib/dialog';
+import '@refinitiv-ui/elements/lib/text-field';
+import '@refinitiv-ui/elements/lib/email-field';
+import '@refinitiv-ui/elements/lib/radio-button';
+import '@refinitiv-ui/elements/lib/checkbox';
+import '@refinitiv-ui/elements/lib/datetime-picker';
+import '@refinitiv-ui/elements/lib/overlay';
+import '@refinitiv-ui/elements/lib/overlay-menu';
+import '@refinitiv-ui/elements/lib/tab-bar';
+import '@refinitiv-ui/elements/lib/tab';
 
 import Sidebar from './components/Sidebar';
 import Chart from './components/Chart';
