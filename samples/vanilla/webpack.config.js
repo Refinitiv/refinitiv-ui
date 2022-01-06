6c374112d1a0dfc4fa186157079c77c6430166a5
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2022-01-06 11:49:46 +0700

    fix: add missing resetActive

--- samples/vanilla/webpack.config.js
+++ samples/vanilla/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, './'),
+      directory: path.resolve(__dirname, './'),
     },
     compress: true
   }
