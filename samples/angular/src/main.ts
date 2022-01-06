8d81cf23b1d603ea987f4495bb7de06939dea71a
Jidapa-Pai <jidapa.nunkaew@refinitiv.com>
2022-01-06 19:13:32 +0700

    docs: init angular sample app

---
+++ samples/angular/src/main.ts
@@ -0,0 +1,31 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+// Components
+import '@refinitiv-ui/elements/lib/button';
+import '@refinitiv-ui/elements/lib/loader';
+import '@refinitiv-ui/elements/lib/text-field';
+import '@refinitiv-ui/elements/lib/password-field';
+import '@refinitiv-ui/elements/lib/header';
+import '@refinitiv-ui/elements/lib/tab-bar';
+import '@refinitiv-ui/elements/lib/tab';
+import '@refinitiv-ui/elements/lib/interactive-chart';
+import '@refinitiv-ui/elements/lib/radio-button';
+import '@refinitiv-ui/elements/lib/datetime-picker';
+import '@refinitiv-ui/elements/lib/email-field';
+import '@refinitiv-ui/elements/lib/overlay-menu';
+import '@refinitiv-ui/elements/lib/dialog';
+import '@refinitiv-ui/elements/lib/toggle';
+import '@refinitiv-ui/elements/lib/checkbox';
+import '@refinitiv-ui/elements/lib/item';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  // eslint-disable-next-line no-console
+  .catch(err => console.error(err));
