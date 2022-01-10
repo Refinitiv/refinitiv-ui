c8eacd4d9ba0ae4ab2de8d548a7bb3b422331e02
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2022-01-10 08:48:33 +0700

    refactor: move main code to index.js

--- samples/vanilla/src/index.js
+++ samples/vanilla/src/index.js
@@ -14,6 +14,8 @@ import '@refinitiv-ui/elements/overlay-menu';
 import '@refinitiv-ui/elements/tab-bar';
 import '@refinitiv-ui/elements/tab';
 
+import { data } from './chartData';
+
 const loadTheme = () => {
   return new Promise(resolve => {
     const theme = sessionStorage.getItem('elf-theme') || 'light';
@@ -22,4 +24,140 @@ const loadTheme = () => {
     resolve(theme);
   })
 }
-loadTheme().then((theme) => {});
+
+window.onload = () => {
+  const container = document.querySelector('.container');
+  const toggle = document.querySelector('ef-toggle');
+  const tabs = document.querySelectorAll('ef-tab');
+  const profileButton = document.querySelector('#profile-button');
+  const dialog = document.querySelector('ef-dialog');
+  const textField = document.querySelector('ef-text-field');
+  const radioGroup = document.querySelector('#radio-group');
+  const datetimePicker = document.querySelector('ef-datetime-picker');
+  const textArea = document.querySelector('textarea');
+  const emailField = document.querySelector('ef-email-field');
+  const menu = document.querySelector('ef-overlay-menu');
+  const jobPanel = document.querySelector('#job-panel');
+  const jobDisplay = document.querySelector('#job-display');
+  const checkbox = document.querySelector('ef-checkbox');
+  const confirmButton = document.querySelector('#confirm-button');
+  const cancelButton = document.querySelector('#cancel-button');
+  const chart = document.querySelector('ef-interactive-chart');
+  loadTheme().then((theme) => {
+
+    // inject theme
+    const head = document.getElementsByTagName('HEAD')[0];
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = `./dist/styles/${theme}.css`;
+    head.appendChild(link);
+
+    container.classList.remove('loading');
+
+    // initial chart config
+    const initialChartType = 'line';
+    customElements.whenDefined('ef-interactive-chart').then(() => {
+      setChartType(initialChartType);
+      setActiveTab(initialChartType);
+    })
+    
+    // initial form config
+    let formData = { isReceiveMail: true };
+    let isEmailError = true;
+    const isSubmitDisabled = (data) => !data.name || !data.email || isEmailError ? true : undefined;
+    confirmButton.disabled = isSubmitDisabled(formData);
+    menu.positionTarget = jobPanel;
+
+    // initial form config theme switching section
+    if (theme === 'dark') {
+      toggle.checked = true;
+    }
+
+    // attach events 
+    tabs.forEach((tab) => {
+      tab.addEventListener('tap', (e) => handleClickTab(e.target));
+    })
+    profileButton.addEventListener('tap', (e) => toggleDialog());
+    textField.addEventListener('value-changed', (e) => handleChangeFormData({ name: e.detail.value }));
+    radioGroup.addEventListener('checked-changed', (e) => handleChangeFormData({ gender: e.target.textContent }));
+    datetimePicker.addEventListener('value-changed', (e) => handleChangeFormData({ date: e.detail.value }));
+    textArea.addEventListener('change', (e) => handleChangeFormData({ address: e.target.value }));
+    emailField.addEventListener('value-changed', (e) => handleChangeFormData({ email: e.detail.value }));
+    emailField.addEventListener('error-changed', (e) => handleEmailError(e.detail.value));
+    menu.parentElement.addEventListener('item-trigger', (e) => handleItemTrigger(e.detail.value));
+    checkbox.checked = true;
+    checkbox.addEventListener('checked-changed', (e) => handleChangeFormData({ isReceiveMail: e.target.checked }));
+    confirmButton.addEventListener('tap', (e) => handleClickConfirm());
+    cancelButton.addEventListener('tap', (e) => toggleDialog());
+    jobPanel.addEventListener('tap', () => {
+      menu.opened = true;
+    });
+    toggle.addEventListener('tap', (e) => {
+      const theme = e.target.checked ? 'dark' : 'light';
+      window.location.reload();
+      sessionStorage.setItem('elf-theme', theme);
+    });
+
+    function setChartType (type) {
+      const config = {
+        options: {
+          timeScale: {
+            timeVisible: true,
+            secondsVisible: true
+          }
+        },
+        series: [{
+          symbol: 'Price',
+          type: type,
+          data: data
+        }]
+      };
+      chart.config = config;
+    };
+
+    function getType (label) {
+      return label.substring(0, label.indexOf(' ')).toLowerCase();
+    }
+    
+    function setActiveTab (selectedType) {
+      tabs.forEach((tab) => {
+        console.log(tab, tab.label)
+        const type = getType(tab.label);
+        (selectedType === type) ? tab.active = true : tab.active = false
+      })
+    }
+    
+    function handleItemTrigger (value) {
+      if(value) {
+        handleChangeFormData({ job: value });
+        jobDisplay.innerHTML = value;
+        menu.opened = false;
+      }
+    };
+
+    function handleClickConfirm () {
+      // eslint-disable-next-line no-console
+      console.log('data =', JSON.stringify(formData, null, 2));
+      toggleDialog();
+    };
+  
+    function handleClickTab (element) {
+      const selectType = getType(element.label);
+      setActiveTab(selectType);
+      setChartType(type);
+    }
+
+    function handleChangeFormData (data) {
+      formData = Object.assign(formData, data);
+      confirmButton.disabled = isSubmitDisabled(formData);
+    };
+
+    function handleEmailError (value) {
+      isEmailError = value
+    };
+
+    function toggleDialog () {
+      dialog.opened = !dialog.opened;
+    };
+  });
+};
--- samples/vanilla/webpack.config.js
+++ samples/vanilla/webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
     filename: "index.js",
     path: path.resolve(__dirname, "dist")
   },
+  // devtool: "inline-source-map", add this if want to debug
   devServer: {
     static: {
       directory: path.resolve(__dirname, './'),
-    },
-    compress: true
+    }
   }
 };
