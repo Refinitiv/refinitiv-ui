9685d891923c2b89cebf044aabfb6dda4b225f41
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2022-01-10 16:40:25 +0700

    refactor: clean up index.js

--- samples/vanilla/src/index.js
+++ samples/vanilla/src/index.js
@@ -25,8 +25,7 @@ const loadTheme = () => {
   })
 }
 
-window.onload = () => {
-  const container = document.querySelector('.container');
+loadTheme().then((defaultTheme) => {
   const toggle = document.querySelector('ef-toggle');
   const tabs = document.querySelectorAll('ef-tab');
   const profileButton = document.querySelector('#profile-button');
@@ -43,121 +42,121 @@ window.onload = () => {
   const confirmButton = document.querySelector('#confirm-button');
   const cancelButton = document.querySelector('#cancel-button');
   const chart = document.querySelector('ef-interactive-chart');
-  loadTheme().then((theme) => {
+  // variable
+  const initialChartType = 'line';
+  let formData = { isReceiveMail: true };
+  let isEmailError = true;
 
-    // inject theme
-    const head = document.getElementsByTagName('HEAD')[0];
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = `./dist/styles/${theme}.css`;
-    head.appendChild(link);
+  // inject theme
+  const head = document.getElementsByTagName('HEAD')[0];
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = `./dist/styles/${defaultTheme}.css`;
+  head.appendChild(link);
 
-    container.classList.remove('loading');
-
-    // initial chart config
-    const initialChartType = 'line';
-    customElements.whenDefined('ef-interactive-chart').then(() => {
-      setChartType(initialChartType);
-      setActiveTab(initialChartType);
-    })
-    
-    // initial form config
-    let formData = { isReceiveMail: true };
-    let isEmailError = true;
-    const isSubmitDisabled = (data) => !data.name || !data.email || isEmailError ? true : undefined;
-    confirmButton.disabled = isSubmitDisabled(formData);
-    menu.positionTarget = jobPanel;
-
-    // initial form config theme switching section
-    if (theme === 'dark') {
-      toggle.checked = true;
-    }
-
-    // attach events 
-    tabs.forEach((tab) => {
-      tab.addEventListener('tap', (e) => handleClickTab(e.target));
-    })
-    profileButton.addEventListener('tap', (e) => toggleDialog());
-    textField.addEventListener('value-changed', (e) => handleChangeFormData({ name: e.detail.value }));
-    radioGroup.addEventListener('checked-changed', (e) => handleChangeFormData({ gender: e.target.textContent }));
-    datetimePicker.addEventListener('value-changed', (e) => handleChangeFormData({ date: e.detail.value }));
-    textArea.addEventListener('change', (e) => handleChangeFormData({ address: e.target.value }));
-    emailField.addEventListener('value-changed', (e) => handleChangeFormData({ email: e.detail.value }));
-    emailField.addEventListener('error-changed', (e) => handleEmailError(e.detail.value));
-    menu.parentElement.addEventListener('item-trigger', (e) => handleItemTrigger(e.detail.value));
-    checkbox.checked = true;
-    checkbox.addEventListener('checked-changed', (e) => handleChangeFormData({ isReceiveMail: e.target.checked }));
-    confirmButton.addEventListener('tap', (e) => handleClickConfirm());
-    cancelButton.addEventListener('tap', (e) => toggleDialog());
-    jobPanel.addEventListener('tap', () => {
-      menu.opened = true;
-    });
-    toggle.addEventListener('tap', (e) => {
-      const theme = e.target.checked ? 'dark' : 'light';
-      window.location.reload();
-      sessionStorage.setItem('elf-theme', theme);
-    });
-
-    function setChartType (type) {
-      const config = {
-        options: {
-          timeScale: {
-            timeVisible: true,
-            secondsVisible: true
-          }
-        },
-        series: [{
-          symbol: 'Price',
-          type: type,
-          data: data
-        }]
-      };
-      chart.config = config;
-    };
-
-    function getType (label) {
-      return label.substring(0, label.indexOf(' ')).toLowerCase();
-    }
-    
-    function setActiveTab (selectedType) {
-      tabs.forEach((tab) => {
-        console.log(tab, tab.label)
-        const type = getType(tab.label);
-        (selectedType === type) ? tab.active = true : tab.active = false
-      })
-    }
-    
-    function handleItemTrigger (value) {
-      if(value) {
-        handleChangeFormData({ job: value });
-        jobDisplay.innerHTML = value;
-        menu.opened = false;
-      }
-    };
-
-    function handleClickConfirm () {
-      // eslint-disable-next-line no-console
-      console.log('data =', JSON.stringify(formData, null, 2));
-      toggleDialog();
-    };
+  // initial chart config
+  setChartType(initialChartType);
+  setActiveTab(initialChartType);
   
-    function handleClickTab (element) {
-      const selectType = getType(element.label);
-      setActiveTab(selectType);
-      setChartType(type);
-    }
+  // initial form config
+  confirmButton.disabled = isSubmitDisabled(formData);
+  menu.positionTarget = jobPanel;
 
-    function handleChangeFormData (data) {
-      formData = Object.assign(formData, data);
-      confirmButton.disabled = isSubmitDisabled(formData);
-    };
+  // initial form config theme switching section
+  if (defaultTheme === 'dark') {
+    toggle.checked = true;
+  }
 
-    function handleEmailError (value) {
-      isEmailError = value
-    };
-
-    function toggleDialog () {
-      dialog.opened = !dialog.opened;
-    };
+  // attach events
+  tabs.forEach((tab) => {
+    tab.addEventListener('tap', (e) => handleClickTab(e.target));
   });
-};
+  profileButton.addEventListener('tap', (e) => toggleDialog());
+  textField.addEventListener('value-changed', (e) => handleChangeFormData({ name: e.detail.value }));
+  radioGroup.addEventListener('checked-changed', (e) => handleChangeFormData({ gender: e.target.textContent }));
+  datetimePicker.addEventListener('value-changed', (e) => handleChangeFormData({ date: e.detail.value }));
+  textArea.addEventListener('change', (e) => handleChangeFormData({ address: e.target.value }));
+  emailField.addEventListener('value-changed', (e) => handleChangeFormData({ email: e.detail.value }));
+  emailField.addEventListener('error-changed', (e) => handleEmailError(e.detail.value));
+  menu.parentElement.addEventListener('item-trigger', (e) => handleItemTrigger(e.detail.value));
+  checkbox.checked = true;
+  checkbox.addEventListener('checked-changed', (e) => handleChangeFormData({ isReceiveMail: e.target.checked }));
+  confirmButton.addEventListener('tap', (e) => handleClickConfirm());
+  cancelButton.addEventListener('tap', (e) => toggleDialog());
+  jobPanel.addEventListener('tap', () => {
+    menu.opened = true;
+  });
+  toggle.addEventListener('checked-changed', (e) => handleChangeTheme(e.target.checked));
+
+  function setChartType (type) {
+    const config = {
+      options: {
+        timeScale: {
+          timeVisible: true,
+          secondsVisible: true
+        }
+      },
+      series: [{
+        symbol: 'Price',
+        type: type,
+        data: data
+      }]
+    };
+    chart.config = config;
+  };
+
+  function getType (label) {
+    return label.substring(0, label.indexOf(' ')).toLowerCase();
+  };
+  
+  function setActiveTab (selectedType) {
+    tabs.forEach((tab) => {
+      const type = getType(tab.label || tab.getAttribute('label'));
+      (selectedType === type) ? tab.active = true : tab.active = false
+    })
+  };
+
+  function isSubmitDisabled (data) {
+    return !data.name || !data.email || isEmailError ? true : undefined
+  };
+
+  function handleItemTrigger (value) {
+    if(value) {
+      handleChangeFormData({ job: value });
+      jobDisplay.innerHTML = value;
+      menu.opened = false;
+    }
+  };
+
+  function handleClickConfirm () {
+    // eslint-disable-next-line no-console
+    console.log('data =', JSON.stringify(formData, null, 2));
+    toggleDialog();
+  };
+
+  function handleClickTab (element) {
+    const selectType = getType(element.label);
+    setActiveTab(selectType);
+    setChartType(selectType);
+  };
+
+  function handleChangeFormData (data) {
+    formData = Object.assign(formData, data);
+    confirmButton.disabled = isSubmitDisabled(formData);
+  };
+
+  function handleEmailError (value) {
+    isEmailError = value;
+    confirmButton.disabled = isSubmitDisabled(formData);
+  };
+
+  function toggleDialog () {
+    dialog.opened = !dialog.opened;
+  };
+
+  function handleChangeTheme (checked) {
+    const theme = checked ? 'dark' : 'light';
+    sessionStorage.setItem('elf-theme', theme);
+    window.location.reload();
+  };
+});
--- samples/vanilla/webpack.config.js
+++ samples/vanilla/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     filename: "index.js",
     path: path.resolve(__dirname, "dist")
   },
-  // devtool: "inline-source-map", add this if want to debug
+  // devtool: "inline-source-map", // add this if want to debug
   devServer: {
     static: {
       directory: path.resolve(__dirname, './'),
