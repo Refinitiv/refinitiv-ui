5267c42deff83235a992b78d4d570c8b8de256bc
Sarin-Udompanish <86759822+Sarin-Udompanish@users.noreply.github.com>
2022-01-11 15:21:35 +0700

    Chore/add framework integration samples (#153)
    * Chore/sample react app (#148)
    
    * docs: init react sample app
    
    * feat: add form handler
    
    * fix: change native styles import path
    
    * chore: change import path
    
    * test: revise test case
    
    * test: fix wrong expect value test case
    
    * test: change test case name
    
    * chore: fix intent and code styling
    
    * chore: change import path
    
    * chore: remove logo file
    
    * docs: init vanilla sample app (#152)
    
    * chore: sample angular app (#150)
    
    * docs: init angular sample app
    
    * chore: sample vue app
    
    Co-authored-by: Theeraphat-Sorasetsakul <86758473+Theeraphat-Sorasetsakul@users.noreply.github.com>
    Co-authored-by: Jidapa-Pai <91583604+Jidapa-Pai@users.noreply.github.com>
    Co-authored-by: Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>

---
+++ samples/angular/src/app/components/profile-form/profile-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileFormComponent } from './profile-form.component';
+
+describe('ProfileFormComponent', () => {
+  let component: ProfileFormComponent;
+  let fixture: ComponentFixture<ProfileFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfileFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfileFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ samples/angular/src/themes/light.js
@@ -0,0 +1,17 @@
+// Native Themes
+import '@refinitiv-ui/halo-theme/light/imports/native-elements';
+// Themes
+import '@refinitiv-ui/elements/lib/button/themes/halo/light';
+import '@refinitiv-ui/elements/lib/text-field/themes/halo/light';
+import '@refinitiv-ui/elements/lib/password-field/themes/halo/light';
+import '@refinitiv-ui/elements/lib/header/themes/halo/light';
+import '@refinitiv-ui/elements/lib/tab-bar/themes/halo/light';
+import '@refinitiv-ui/elements/lib/tab/themes/halo/light';
+import '@refinitiv-ui/elements/lib/interactive-chart/themes/halo/light';
+import '@refinitiv-ui/elements/lib/radio-button/themes/halo/light';
+import '@refinitiv-ui/elements/lib/datetime-picker/themes/halo/light';
+import '@refinitiv-ui/elements/lib/email-field/themes/halo/light';
+import '@refinitiv-ui/elements/lib/overlay-menu/themes/halo/light';
+import '@refinitiv-ui/elements/lib/dialog/themes/halo/light';
+import '@refinitiv-ui/elements/lib/toggle/themes/halo/light';
+import '@refinitiv-ui/elements/lib/checkbox/themes/halo/light';
