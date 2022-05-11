1b9546b4213d8be7222a0b517571878d5e436d45
Sakchai Homhual <Sakchai.Homhual@refinitiv.com>
2022-05-11 16:33:52 +0700

    feat(phrasebook): add translation for card element

---
+++ packages/phrasebook/src/locale/de/card.ts
@@ -0,0 +1,10 @@
+import { Phrasebook } from '../../translation.js';
+import './shared.js';
+
+const translations = {
+  MENU: 'menu'
+};
+
+Phrasebook.define('de', 'ef-card', translations);
+
+export default translations;
---
+++ packages/phrasebook/src/locale/en/card.ts
@@ -0,0 +1,10 @@
+import { Phrasebook } from '../../translation.js';
+import './shared.js';
+
+const translations = {
+  MENU: 'menu'
+};
+
+Phrasebook.define('en', 'ef-card', translations);
+
+export default translations;
