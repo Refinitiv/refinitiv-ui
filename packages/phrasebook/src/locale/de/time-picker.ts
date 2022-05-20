723dfa5a7d6662bb14a44aa09f3de2d397f95208
akiko-shibano <105897650+akiko-shibano@users.noreply.github.com>
2022-05-20 16:10:42 +0900

    Update time-picker.ts

--- packages/phrasebook/src/locale/de/time-picker.ts
+++ packages/phrasebook/src/locale/de/time-picker.ts
@@ -1,9 +1,9 @@
 import { Phrasebook } from '../../translation.js';
 
 const translations = {
-  BEFORE_MIDDAY: 'Before Midday',
-  AFTER_MIDDAY: 'After Midday',
-  TOGGLE_TIME_PERIOD: 'Toggle Time Period',
+  BEFORE_MIDDAY: 'Vormittags',
+  AFTER_MIDDAY: 'Nachmittags',
+  TOGGLE_TIME_PERIOD: ' Zeitraum wechseln',
   SELECT_HOURS: '{value, select, null {Select hours} other {{value, plural, =1 {# hour} other {# hours}}}}',
   SELECT_MINUTES: '{value, select, null {Select minutes} other {{value, plural, =1 {# minute} other {# minutes}}}}',
   SELECT_SECONDS: '{value, select, null {Select seconds} other {{value, plural, =1 {# second} other {# seconds}}}}',
