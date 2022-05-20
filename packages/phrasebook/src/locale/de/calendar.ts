d878361a6706ad27329b745e8931ecc3220b4911
akiko-shibano <105897650+akiko-shibano@users.noreply.github.com>
2022-05-20 16:34:47 +0900

    Update calendar.ts

--- packages/phrasebook/src/locale/de/calendar.ts
+++ packages/phrasebook/src/locale/de/calendar.ts
@@ -14,7 +14,7 @@ const translations = {
   NOW: '{view, select, year {Current year, {value, date, ::yyyy}} month {Current month, {value, date, ::MMMM-yyyy}} other {Today, {value, date, full}}}',
   SELECTED_NOW: 'Ausgewählt: {view, select, year {current year, {value, date, ::yyyy}} month {current month, {value, date, ::MMMM-yyyy}} other {today, {value, date, full}}}',
   SELECTED_DATE: 'Ausgewählt {count, plural, =1 {date is {value, date, full}} other {# dates, {value, date, full} and others}}',
-  SELECTED_RANGE: ' Der ausgewählte Bereich reicht von {from, date, full}{to, select, null {} other { to {to, date, full}}}',
+  SELECTED_RANGE: 'Der ausgewählte Bereich reicht von {from, date, full}{to, select, null {} other { to {to, date, full}}}',
   SELECTED_NONE: 'Keine ausgewählt. Wählen Sie {range, select, true {date range} other {{multiple, select, true {dates} other {date}}}}'
 };
 
