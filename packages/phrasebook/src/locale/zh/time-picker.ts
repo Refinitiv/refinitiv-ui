a410fa11c140afb31f6567163a85d3054e41c7a4
danlei-qu <106130380+danlei-qu@users.noreply.github.com>
2022-05-24 11:32:35 +0800

    Simplified Chinese updated. (#323)

--- packages/phrasebook/src/locale/zh/time-picker.ts
+++ packages/phrasebook/src/locale/zh/time-picker.ts
@@ -1,13 +1,13 @@
 import { Phrasebook } from '../../translation.js';
 
 const translations = {
-  BEFORE_MIDDAY: 'Before Midday',
-  AFTER_MIDDAY: 'After Midday',
-  TOGGLE_TIME_PERIOD: 'Toggle Time Period',
-  SELECT_HOURS: '{value, select, null {Select hours} other {{value, plural, =1 {# hour} other {# hours}}}}',
-  SELECT_MINUTES: '{value, select, null {Select minutes} other {{value, plural, =1 {# minute} other {# minutes}}}}',
-  SELECT_SECONDS: '{value, select, null {Select seconds} other {{value, plural, =1 {# second} other {# seconds}}}}',
-  SELECTED: '{value, select, null {Selected none. Choose time} other {Selected time is: {showSeconds, select, true {{amPm, select, true {{value, time, ::hmsa}} other {{value, time, ::Hms}}}} other {{amPm, select, true {{value, time, ::hma}} other {{value, time, ::Hm}}}}}}}'
+  BEFORE_MIDDAY: '中午之前',
+  AFTER_MIDDAY: '中午之后',
+  TOGGLE_TIME_PERIOD: '切换时间期间',
+  SELECT_HOURS: '{value, select, null {选择小时} other {{value, plural, =1 {个小时} other {个小时}}}}',
+  SELECT_MINUTES: '{value, select, null {选择分钟} other {{value, plural, =1 {分钟} other {分钟}}}}',
+  SELECT_SECONDS: '{value, select, null {选择秒钟} other {{value, plural, =1 {秒钟} other {秒钟}}}}',
+  SELECTED: '{value, select, null {未选择。请选择时间} other {所选时间是: {showSeconds, select, true {{amPm, select, true {{value, time, ::hmsa}} other {{value, time, ::Hms}}}} other {{amPm, select, true {{value, time, ::hma}} other {{value, time, ::Hm}}}}}}}'
 };
 
 Phrasebook.define('zh', 'ef-time-picker', translations);
