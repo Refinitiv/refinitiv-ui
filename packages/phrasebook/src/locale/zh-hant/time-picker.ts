694915070935dfe806bb8e29d9dd17a04af8bc80
Domrongpon Tanpaibul <domrongpon.tanpaibul@refinitiv.com>
2022-05-24 14:19:51 +0700

    chore: fix missing # from chinese translation

--- packages/phrasebook/src/locale/zh-hant/time-picker.ts
+++ packages/phrasebook/src/locale/zh-hant/time-picker.ts
@@ -4,9 +4,9 @@ const translations = {
   BEFORE_MIDDAY: '中午之前',
   AFTER_MIDDAY: '中午之後',
   TOGGLE_TIME_PERIOD: '切換時間期間',
-  SELECT_HOURS: '{value, select, null {選擇小時} other {{value, plural, =1 {個小時} other {個小時}}}}',
-  SELECT_MINUTES: '{value, select, null {選擇分鐘} other {{value, plural, =1 {分鐘} other {分鐘}}}}',
-  SELECT_SECONDS: '{value, select, null {選擇秒鐘} other {{value, plural, =1 {秒鐘} other {秒鐘}}}}',
+  SELECT_HOURS: '{value, select, null {選擇小時} other {{value, plural, =1 {# 個小時} other {# 個小時}}}}',
+  SELECT_MINUTES: '{value, select, null {選擇分鐘} other {{value, plural, =1 {# 分鐘} other {# 分鐘}}}}',
+  SELECT_SECONDS: '{value, select, null {選擇秒鐘} other {{value, plural, =1 {# 秒鐘} other {# 秒鐘}}}}',
   SELECTED: '{value, select, null {未選擇。請選擇時間} other {所選時間是: {showSeconds, select, true {{amPm, select, true {{value, time, ::hmsa}} other {{value, time, ::Hms}}}} other {{amPm, select, true {{value, time, ::hma}} other {{value, time, ::Hm}}}}}}}'
 };
 
--- packages/phrasebook/src/locale/zh/time-picker.ts
+++ packages/phrasebook/src/locale/zh/time-picker.ts
@@ -4,9 +4,9 @@ const translations = {
   BEFORE_MIDDAY: '中午之前',
   AFTER_MIDDAY: '中午之后',
   TOGGLE_TIME_PERIOD: '切换时间期间',
-  SELECT_HOURS: '{value, select, null {选择小时} other {{value, plural, =1 {个小时} other {个小时}}}}',
-  SELECT_MINUTES: '{value, select, null {选择分钟} other {{value, plural, =1 {分钟} other {分钟}}}}',
-  SELECT_SECONDS: '{value, select, null {选择秒钟} other {{value, plural, =1 {秒钟} other {秒钟}}}}',
+  SELECT_HOURS: '{value, select, null {选择小时} other {{value, plural, =1 {# 个小时} other {# 个小时}}}}',
+  SELECT_MINUTES: '{value, select, null {选择分钟} other {{value, plural, =1 {# 分钟} other {# 分钟}}}}',
+  SELECT_SECONDS: '{value, select, null {选择秒钟} other {{value, plural, =1 {# 秒钟} other {# 秒钟}}}}',
   SELECTED: '{value, select, null {未选择。请选择时间} other {所选时间是: {showSeconds, select, true {{amPm, select, true {{value, time, ::hmsa}} other {{value, time, ::Hms}}}} other {{amPm, select, true {{value, time, ::hma}} other {{value, time, ::Hm}}}}}}}'
 };
 
