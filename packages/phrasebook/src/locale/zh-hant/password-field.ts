3c32b85e381918dcb2fa6b762361ad6bd3a841f0
danlei-qu <106130380+danlei-qu@users.noreply.github.com>
2022-05-24 10:47:48 +0800

    Traditional Chinese updated.

--- packages/phrasebook/src/locale/zh-hant/password-field.ts
+++ packages/phrasebook/src/locale/zh-hant/password-field.ts
@@ -1,8 +1,8 @@
 import { Phrasebook } from '../../translation.js';
 
 const translations = {
-  SHOW_PASSWORD: 'Show password',
-  HIDE_PASSWORD: 'Hide password'
+  SHOW_PASSWORD: '顯示密碼',
+  HIDE_PASSWORD: '隱藏密碼'
 };
 
 Phrasebook.define('zh-Hant', 'ef-password-field', translations);
