ba9cc8d563444dba35a082a3898dc4e8f9235074
Wasuwat Limsuparhat <86233706+wsuwt@users.noreply.github.com>
2022-06-07 10:47:09 +0700

    feat(slider): add localised label for slider thumb (#351)
    * feat(slider): add localised label for slider thumb
    
    * tests: update snapshots
    
    Co-authored-by: wsuwt <wasuwat.limsuparhat@refinitiv.com>

---
+++ packages/phrasebook/src/locale/de/slider.ts
@@ -0,0 +1,11 @@
+import { Phrasebook } from '../../translation.js';
+
+const translations = {
+  FROM: 'From',
+  TO: 'To',
+  VALUE: 'Value'
+};
+
+Phrasebook.define('de', 'ef-slider', translations);
+
+export default translations;
