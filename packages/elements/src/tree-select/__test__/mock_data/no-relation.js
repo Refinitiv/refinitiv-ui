ea88b3c1389ef7887af890206bdd29695ce49787
Sarin-Udompanish <86759822+Sarin-Udompanish@users.noreply.github.com>
2022-06-15 10:35:27 +0700

    fix(tree-select): show unselected item on selection filter (#358)
    * fix(tree-select): show unselected item on selection filter
    
    * test(tree-select): change test case name
    
    * fix(tree-select): correcting memo data
    
    * test(tree-select): typo test case name
    
    * fix(tree-select): change filter condition to make query correct in selection filter
    
    * docs(tree-select): add comment to selected filter conditions
    
    * test(tree-select): update mock data filename to kebab case
    
    Co-authored-by: Sakchai Homhual <Sakchai.Homhual@refinitiv.com>

---
+++ packages/elements/src/tree-select/__test__/mock_data/no-relation.js
@@ -0,0 +1,19 @@
+export const noRelationData = [
+  {
+    'label': 'Number 1',
+    'value': '1',
+    'selected': true,
+    'items': [
+      {
+        'label': 'Number 1.1',
+        'value': '1.1',
+        'selected': true,
+      },
+      {
+        'label': 'Number 1.2',
+        'value': '1.2',
+        'selected': false,
+      }
+    ],
+  }
+];
