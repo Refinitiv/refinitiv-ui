e95fab92fa2ef53198c2f81b0beb9c2e6e5f1330
Sakchai Homhual <87511654+Sakchai-Refinitiv@users.noreply.github.com>
2022-06-20 11:51:54 +0700

    ci: fix test timeout fails because ci performance drop (#368)
    ci: fix test coverage fails because ci performance drop
    ci: fix sonarcloud coverage
    ci: set browserstack maximum timeout
    ci: test coverage on chrome only

--- karma.config.js
+++ karma.config.js
@@ -236,7 +236,7 @@ if (argv.browserstack && !argv.watch) {
     build: process.env.BROWSERSTACK_BUILD,
     project: process.env.BROWSERSTACK_PROJECT_NAME || 'Refinitiv UI',
     name: packageName,
-    timeout: 1800,
+    timeout: 1800, // Maximum
     retryLimit: 0
   };
   reporters.push('BrowserStack');
