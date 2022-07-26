02da84f515296a62f4a5312eecb6ecad553a6c33
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2022-07-26 18:56:19 +0700

    fix(utils): fix export variable in localCache

--- packages/utils/src/cache.ts
+++ packages/utils/src/cache.ts
@@ -1,4 +1,4 @@
 export { CacheLocalStorage } from './cache/cache-localstorage.js';
-export { CacheIndexedDBStorage, DBConfig, IndexedDBDatabase } from './cache/cache-indexeddb.js';
-export { CacheItem } from './cache/types.js';
+export { CacheIndexedDBStorage } from './cache/cache-indexeddb.js';
+export { CacheItem, CacheMap, CacheIndexedDBStorageConfig, CacheStorage } from './cache/types.js';
 export { LocalCache } from './cache/local-cache.js';
