04d16d802848144141c985373dbf5a56247cfa62
Sarin-Udompanish <86759822+Sarin-Udompanish@users.noreply.github.com>
2022-09-26 13:48:02 +0700

    feat(create-efx): add create-efx package (#450)
    * feat(create-efx): init project
    
    * refactor(create-efx): revise renamer class code
    
    * refactor(create-efx): refactor utils code
    
    * fix(create-efx): add more error message
    
    * fix(create-efx): change copy path
    
    * chore(create-efx): fix eslint
    
    * chore: update package.json
    
    * fix(create-efx): remove unnecessary files
    
    * feat(create-efx): add help and version commands
    
    * chore(create-efx): move dependencies to devDependencies
    
    * fix(create-efx): remove unused variable
    
    * fix(create-efx): fix version command
    
    * fix(create-efx): typo
    
    * fix(create-efx): add test plugin
    
    * fix(create-efx): change version command
    
    * fix(create-efx): validate project name that start with hyphen
    
    * fix(create-efx): add more validation
    
    * chore(create-efx): update typescript version of template
    
    * chore(create-efx): remove extension rule from eslint template
    
    * fix(create-efx): add template prefix to all files in template folder
    
    Co-authored-by: Sarin Udompanish <sarin.udompanish2@lseg.com>
    Co-authored-by: Wasuwat Limsuparhat <86233706+wsuwt@users.noreply.github.com>

---
+++ packages/create-efx/src/template/test/efx-element.test.js
@@ -0,0 +1,18 @@
+// The test runner can be configured with a configuration file (web-test-runner.config.js).
+// https://modern-web.dev/docs/test-runner/overview/
+import { fixture, expect } from '@refinitiv-ui/test-helpers';
+
+// import element and theme
+import '../src/efx-element.ts';
+import '../themes/halo-theme/dark';
+
+describe('EfxElementTest', () => {
+  it('Label and DOM structure is correct', async () => {
+    const el = await fixture('<efx-element></efx-element>');
+    await expect(el).shadowDom.to.equalSnapshot({ ignoreAttributes: ['class', 'style'] });
+    expect(el.count).to.equal(0);
+    const button = el.shadowRoot.querySelector('[part=button]');
+    button.click();
+    expect(el.count).to.equal(1);
+  });
+});
