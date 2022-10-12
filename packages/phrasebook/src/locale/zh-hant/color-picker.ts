8c4f12e8d0cf15ed17f4fb6bdadb3f6bdf488a33
nantawat.poothong <nantawat.poothong@lseg.com>
2022-10-12 15:18:29 +0700

    fix(util): remove code not related to color util

--- packages/phrasebook/src/locale/zh-hant/color-picker.ts
+++ packages/phrasebook/src/locale/zh-hant/color-picker.ts
@@ -4,19 +4,6 @@ import './shared.js';
 import './color-dialog.js';
 
 const translations = {
-  VERY_LIGHT: 'Very light',
-  LIGHT: 'Light',
-  VERY_DARK: 'Very dark',
-  DARK: 'Dark',
-  BLACK: 'Black',
-  WHITE: 'White',
-  WITH: 'With {number}%',
-  YELLOW: 'Yellow',
-  GREEN: 'Green',
-  CYAN: 'Cyan',
-  BLUE: 'Blue',
-  MAGENTA: 'Magenta',
-  RED: 'Red'
 };
 
 Phrasebook.define('zh-hant', 'ef-color-picker', translations);
