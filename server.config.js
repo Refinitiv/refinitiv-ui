a8a90d692c5216fc99abb879dc2d5ef027a22805
Napat Bualoy <napat.bualoy@lseg.com>
2022-10-18 17:55:55 +0700

    chore: remove legacy plugin from server config

--- server.config.js
+++ server.config.js
@@ -3,9 +3,7 @@
  * Generic configuration for web-server.
  * Extend as required
  */
-const { legacyPlugin } = require('@web/dev-server-legacy');
 const { ROOT } = require('./scripts/helpers');
-const path = require('path');
 
 module.exports = {
   rootDir: ROOT,
@@ -14,22 +12,4 @@ module.exports = {
   nodeResolve: true,
   preserveSymlinks: true,
   appIndex: '/',
-  plugins: [
-    legacyPlugin({
-      polyfills: {
-        webcomponents: true,
-        shadyCssCustomStyle: true,
-        // Inject lit's polyfill-support module into test files, which is required
-        // for interfacing with the webcomponents polyfills
-        custom: [
-          {
-            name: 'lit-polyfill-support',
-            path: path.resolve(ROOT, 'node_modules/lit/polyfill-support.js'),
-            test: "!('attachShadow' in Element.prototype)",
-            module: false,
-          }
-        ]
-      }
-    }) // IE11 support
-  ]
 };
