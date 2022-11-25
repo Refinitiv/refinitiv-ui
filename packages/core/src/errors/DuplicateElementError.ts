ebf83a4bf03f8ef2a94ebbdbdc2401c21993cea9
Napat Bualoy <napat.bualoy@lseg.com>
2022-11-25 13:43:42 +0700

    chore: update new link for kb page

--- packages/core/src/errors/DuplicateElementError.ts
+++ packages/core/src/errors/DuplicateElementError.ts
@@ -6,12 +6,17 @@ const generateMessage = (name: string): string =>
 
 [${name}] has already been defined.
 
+Potential causes:
+1. No deduplication task has been performed
+2. The same element definition has been loaded in multiple bundles
+3. A single package has been upgraded, without upgrading other EF dependencies
+
 Recommended fix:
 1. Run 'npm dedupe' in you project folder
 2. Rebuild your project`;
 
 export class DuplicateElementError extends GenericError {
   constructor (name: string) {
-    super(generateMessage(name), 'https://elf.int.refinitiv.com/error/duplicate-element');
+    super(generateMessage(name), 'https://ui.refinitiv.com/kb/duplicate-element');
   }
 }
