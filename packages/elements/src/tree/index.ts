3a03d560308ef6cd2c55d829664f54740d0f2439
Napat Bualoy <napat.bualoy@lseg.com>
2022-12-16 10:52:59 +0700

    feat(tree): export type TreeManagerMode

--- packages/elements/src/tree/index.ts
+++ packages/elements/src/tree/index.ts
@@ -1,7 +1,7 @@
 export * from './elements/tree.js';
 export * from './elements/tree-item.js';
 export { TreeRenderer } from './helpers/renderer.js';
-export { TreeManager } from './managers/tree-manager.js';
+export { TreeManager, TreeManagerMode } from './managers/tree-manager.js';
 
 import type { TreeData, TreeDataItem } from './helpers/types';
 export type { TreeData, TreeDataItem };
