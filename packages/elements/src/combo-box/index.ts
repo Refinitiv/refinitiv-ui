6950c18b9858997c56e9e224ccb7155da613d209
Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>
2023-01-03 10:02:20 +0700

    Update packages/elements/src/combo-box/index.ts
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>

--- packages/elements/src/combo-box/index.ts
+++ packages/elements/src/combo-box/index.ts
@@ -68,7 +68,7 @@ const freeTextMultipleWarning = new WarningNotice('"free-text" mode is not compa
  * @prop {string} [name=''] - Set name of the element
  *
  * @fires value-changed - Fired when the user commits a value change. The event is not triggered if `value` property is changed programmatically.
- * @fires query-changed - Fired when the user changes value in the input to change a query word. If `query-debounce-rate` is set, this event will trigger after the time that you set in `query-debounce-rate`. The event is not triggered if `query` property is changed programmatically.
+ * @fires query-changed - Fired when the user changes value in the input to change a query word. If `query-debounce-rate` is set, this event will be triggered after debounce completion. The event is not triggered if `query` property is changed programmatically.
  * @fires opened-changed - Fired when the user opens or closes control's popup. The event is not triggered if `opened` property is changed programmatically.
  */
 @customElement('ef-combo-box')
