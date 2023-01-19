c64e9678a868df23a02fd6dbff8c2327316bf9d6
Sakchai Homhual <Sakchai.Homhual@refinitiv.com>
2023-01-19 13:28:11 +0700

    test(elements): add code coverage test

--- packages/elements/web-test-runner.config.js
+++ packages/elements/web-test-runner.config.js
@@ -1,13 +1,28 @@
 #!/usr/bin/env node
-const path = require('path');
+const path = require('node:path');
 const { playwrightLauncher } = require('@web/test-runner-playwright');
 const { ROOT, ELEMENTS_ROOT, checkElement } = require('./scripts/helpers');
 const ELEMENT = process.env.ELEMENT;
 const testAll = ELEMENT === 'all' || ELEMENT === undefined;
 
 module.exports = {
-  files: [path.join(ELEMENTS_ROOT, 'src', `${ testAll ? '*' : ELEMENT }/__test__/**/*.test.js`)],
+  files: [
+    path.join(ELEMENTS_ROOT, 'src', `${ testAll ? '*' : ELEMENT }/__test__/**/*.test.js`),
+    '!**/node_modules/**/*', // exclude any node modules
+  ],
   nodeResolve: true,
+  coverage: true,
+  coverageConfig: {
+    include: [path.join(ELEMENTS_ROOT, 'lib', ELEMENT, '/**/*')],
+    report: true,
+    reportDir: path.join('coverage', ELEMENT),
+    threshold: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
   concurrentBrowsers: 3,
   browsers: [
     playwrightLauncher({ product: 'chromium' }, {
