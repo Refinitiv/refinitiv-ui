181c396e542ade4004efd0fb760ae3c4f6d677ba
Napat Bualoy <napat.bualoy@lseg.com>
2023-01-31 18:19:59 +0700

    test(button-bar): update unit test and snapshot to support web test runner

---
+++ packages/elements/src/button-bar/__test__/__snapshots__/button-bar.test.snap.js
@@ -0,0 +1,9 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["button-bar/ButtonBar should be created"] = 
+`<slot>
+</slot>
+`;
+/* end snapshot button-bar/ButtonBar should be created */
+
