aabc0d6517b6bff90f934e821d2e0aba1dc2d3b4
Napat Bualoy <napat.bualoy@lseg.com>
2023-01-31 23:13:16 +0700

    test(progress-bar): update unit test and snapshot to support web test runner

---
+++ packages/elements/src/progress-bar/__test__/__snapshots__/progress-bar.test.snap.js
@@ -0,0 +1,16 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["progress-bar/ProgressBar DOM structure is correct"] = 
+`<div
+  part="bar"
+  style="width:100%;min-width:1px;"
+>
+  <span part="label">
+    <slot name="label">
+    </slot>
+  </span>
+</div>
+`;
+/* end snapshot progress-bar/ProgressBar DOM structure is correct */
+
