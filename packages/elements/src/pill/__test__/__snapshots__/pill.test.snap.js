f162193f1efbd45bda6a9c50b4710e0d4d93b54d
Napat Bualoy <napat.bualoy@lseg.com>
2023-01-31 23:10:35 +0700

    test(pill): update unit test and snapshot to support web test runner

---
+++ packages/elements/src/pill/__test__/__snapshots__/pill.test.snap.js
@@ -0,0 +1,43 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["pill/Pill Should have correct default Shadow DOM structure"] = 
+`<div
+  part="content"
+  role="none"
+>
+  <slot>
+    ...
+  </slot>
+</div>
+`;
+/* end snapshot pill/Pill Should have correct default Shadow DOM structure */
+
+snapshots["pill/Pill Should have correct \"clears\" Shadow DOM structure"] = 
+`<div
+  part="content"
+  role="none"
+>
+  <slot>
+    ...
+  </slot>
+</div>
+<ef-icon
+  aria-hidden="true"
+  icon="cross"
+  part="close"
+>
+</ef-icon>
+`;
+/* end snapshot pill/Pill Should have correct "clears" Shadow DOM structure */
+
+snapshots["pill/Pill Should have correct default Light DOM structure for a slot"] = 
+`Tiger
+`;
+/* end snapshot pill/Pill Should have correct default Light DOM structure for a slot */
+
+snapshots["pill/Pill Should have correct \"clears\" Light DOM structure for a slot"] = 
+`Tiger
+`;
+/* end snapshot pill/Pill Should have correct "clears" Light DOM structure for a slot */
+
