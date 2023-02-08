6f11dae8af0c51310e49560521db372906a7b03e
Napat Bualoy <napat.bualoy@lseg.com>
2023-02-08 15:27:47 +0700

    test(list): update snapshot

--- packages/elements/src/heatmap/__test__/__snapshots__/heatmap.test.snap.js
+++ packages/elements/src/heatmap/__test__/__snapshots__/heatmap.test.snap.js
@@ -1,23 +1,18 @@
-# `heatmap/Heatmap`
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
 
-## `DOM Structure`
-
-####   `DOM structure is correct`
-
-```html
-<div id="container">
+snapshots["heatmap/Heatmap DOM Structure DOM structure is correct"] = 
+`<div id="container">
   <div id="canvas-container">
     <ef-canvas part="canvas">
     </ef-canvas>
   </div>
 </div>
+`;
+/* end snapshot heatmap/Heatmap DOM Structure DOM structure is correct */
 
-```
-
-####   `DOM structure with tooltip config is correct`
-
-```html
-<div id="container">
+snapshots["heatmap/Heatmap DOM Structure DOM structure with tooltip config is correct"] = 
+`<div id="container">
   <div id="y-axis-container">
     <div part="cross-box">
     </div>
@@ -35,13 +30,11 @@
 </div>
 <ef-tooltip role="tooltip">
 </ef-tooltip>
+`;
+/* end snapshot heatmap/Heatmap DOM Structure DOM structure with tooltip config is correct */
 
-```
-
-####   `DOM structure with axes config is correct`
-
-```html
-<div id="container">
+snapshots["heatmap/Heatmap DOM Structure DOM structure with axes config is correct"] = 
+`<div id="container">
   <div id="y-axis-container">
     <div part="cross-box">
     </div>
@@ -55,6 +48,6 @@
     </ef-canvas>
   </div>
 </div>
-
-```
+`;
+/* end snapshot heatmap/Heatmap DOM Structure DOM structure with axes config is correct */
 
