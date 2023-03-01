b8eef09132807ba8ea529fc0cf94bd554b55efef
Sarin Udompanish <sarin.udompanish2@lseg.com>
2023-03-01 11:19:44 +0700

    test(icon, flag): change nextFrame to aTimeout

--- packages/elements/src/icon/__test__/helpers/helpers.js
+++ packages/elements/src/icon/__test__/helpers/helpers.js
@@ -1,4 +1,4 @@
-import { fixture, nextFrame } from "@refinitiv-ui/test-helpers";
+import { fixture, nextFrame, aTimeout } from "@refinitiv-ui/test-helpers";
 
 export const tickSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path stroke="#000" d="M14 4l-8.25 8.25L2 8.5" fill="none" fill-rule="evenodd"></path></svg>';
 export const iconName = 'tick';
@@ -8,7 +8,7 @@ export const createAndWaitForLoad = async template => {
   const el = await fixture(template);
   await nextFrame();
   await nextFrame();
-  await nextFrame();
+  await aTimeout(50);
   return el;
 };
 
