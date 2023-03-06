574c2e3ea5f5a4aeb26016dd143551768fde92b6
wsuwt <wasuwat.limsuparhat2@lseg.com>
2023-03-06 14:04:42 +0700

    feat: add `sub-label` element

---
+++ packages/components/src/sub-label/index.ts
@@ -0,0 +1,63 @@
+import { BasicElement, html, css, TemplateResult, CSSResultGroup } from '@refinitiv-ui/core';
+import { customElement } from '@refinitiv-ui/core/decorators/custom-element.js';
+import { property } from '@refinitiv-ui/core/decorators/property.js';
+import { VERSION } from '../version.js';
+
+@customElement('ds-sub-label')
+export class SubLabel extends BasicElement {
+  /**
+   * Element version number
+   * @returns version number
+   */
+  static get version (): string {
+    return VERSION;
+  }
+
+  /**
+   * A `CSSResultGroup` that will be used
+   * to style the host, slotted children
+   * and the internal template of the element.
+   * @returns CSS template
+   */
+  static get styles (): CSSResultGroup {
+    return css`
+      :host {
+        display: inline-block;
+      }
+      :host[error],
+      :host[error][warning] {
+        color: (--ds-color-error);
+      }
+      :host[warning] {
+        color: (--ds-color-warning);
+      }
+    `;
+  }
+
+  /**
+   * Set state to error
+   */
+  @property({ type: Boolean, reflect: true })
+  public error = false;
+
+  /**
+   * Set state to warning
+   */
+  @property({ type: Boolean, reflect: true })
+  public warning = false;
+
+  /**
+   * A `TemplateResult` that will be used
+   * to render the updated internal template.
+   * @return Render template
+   */
+  protected render (): TemplateResult {
+    return html`<slot></slot>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ds-sub-label': SubLabel;
+  }
+}
