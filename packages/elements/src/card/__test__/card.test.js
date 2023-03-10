e88e0748368804b23f9e7f2e8ac0f86fb03e4fde
nantawat.poothong <nantawat.poothong@lseg.com>
2023-03-10 15:41:11 +0700

    test(card): not close menu when click non value item

--- packages/elements/src/card/__test__/card.test.js
+++ packages/elements/src/card/__test__/card.test.js
@@ -10,7 +10,7 @@ import '@formatjs/intl-pluralrules/locale-data/en';
 import '@refinitiv-ui/elements/card';
 import '@refinitiv-ui/elemental-theme/light/ef-card';
 
-const menuData = [{ label: 'Spain', value: 'Spain'  }, { label: 'France',value: 'France', disabled: true }, { label: 'Italy', value: 'Italy' }];
+const menuData = [{ label: 'Spain', value: 'Spain'  }, { label: 'France',value: 'France', disabled: true }, { label: 'Italy', value: 'Italy' }, { label: 'Other', items: [{ label: 'Thailand', value: 'Thailand' }] }];
 
 describe('card/Card', () => {
   describe('DOM structure', () => {
@@ -81,6 +81,36 @@ describe('card/Card', () => {
 
       await oneEvent(el, 'item-trigger');
       expect(menu.opened).to.equal(false, 'Menu should close when item is selected');
+    });
+    it('Should open menu and not close when clicked on non value item', async () => {
+      const el = await fixture('<ef-card lang="en-gb">Card</ef-card>');
+      el.config = {
+        menu: {
+          data: menuData
+        }
+      };
+      await elementUpdated(el);
+
+      const openMenu = el.openMenuElement;
+      const menu = el.menuElement;
+
+      expect(openMenu, 'Open menu button element does not exist').to.exist;
+      expect(menu, 'Menu element does not exist').to.exist;
+
+      openMenu.click();
+
+      await elementUpdated(el);
+      expect(menu.opened).to.equal(true, 'Menu should open on button click');
+
+      const item = menu.shadowRoot.querySelectorAll('ef-item')[3];
+      
+      expect(item, 'Menu config is not passed correctly').to.exist;
+
+      setTimeout(() => {
+        item.click();
+      });
+      await oneEvent(el, 'item-trigger');
+      expect(menu.opened).to.equal(true, 'Menu should still open');
     });
   });
 
