deafb812a5f42e78030fda68d5b2fb13df07ab20
Sarin Udompanish <sarin.udompanish2@lseg.com>
2023-03-13 10:17:37 +0700

    feat: add select element

---
+++ packages/components/src/select/__test__/select.selected.test.js
@@ -0,0 +1,57 @@
+import { fixture, expect, elementUpdated } from '@refinitiv-ui/test-helpers';
+import { getData, getOptions, openedUpdated, snapshotIgnore } from './utils';
+
+import '@refinitiv-ui/elements/select';
+import '@refinitiv-ui/elemental-theme/light/ef-select';
+
+describe('select/Selection', () => {
+  describe('Selection by Selected Property', () => {
+    it('Options Selected: Afghanistan', async () => {
+      const el = await fixture(`<ef-select opened placeholder="Placeholder">${getOptions([1])}</ef-select>`);
+      await openedUpdated(el);
+      expect(el.value).to.equal('AF', 'Value is not reflected from selected');
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+
+      el.querySelector('ef-item[value=AF]').selected = false;
+      el.querySelector('ef-item[value=AL]').selected = true;
+      await elementUpdated(el);
+      expect(el.value).to.equal('AL', 'Value is not reflected from selected');
+
+      el.querySelector('ef-item[value=AL]').selected = false;
+      await elementUpdated(el);
+      expect(el.value).to.equal('', 'Value is not reset to empty string');
+    });
+
+    it('Data Selected: Afghanistan', async () => {
+      const el = await fixture('<ef-select opened placeholder="Placeholder"></ef-select>');
+      el.data = getData([1]);
+      await openedUpdated(el);
+      expect(el.value).to.equal('AF', 'Value is not reflected from selected');
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+    });
+
+    it('Data Selected change', async () => {
+      const el = await fixture('<ef-select opened placeholder="Placeholder"></ef-select>');
+      el.data = getData([1]);
+      await openedUpdated(el);
+      expect(el.value).to.equal('AF', 'Value is not reflected from selected');
+
+      el.data = getData([4]);
+      await elementUpdated(el);
+      expect(el.value).to.equal('AL', 'Value is not reflected from selected');
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+    });
+
+    it('Data Unselected', async () => {
+      const el = await fixture('<ef-select opened placeholder="Placeholder"></ef-select>');
+      el.data = getData([1]);
+      await openedUpdated(el);
+      expect(el.value).to.equal('AF', 'Value is not reflected from selected');
+
+      el.data = getData();
+      await elementUpdated(el);
+      expect(el.value).to.equal('', 'Value is not reset to empty string');
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+    });
+  });
+});
---
+++ packages/components/src/select/helpers/types.ts
@@ -0,0 +1,4 @@
+import type { ItemData } from '../../sub-item';
+
+export type SelectData = ItemData[];
+export type SelectDataItem= ItemData;
