090e4560036faff698659b433cba6f95db3f4f70
Theeraphat-Sorasetsakul <86758473+Theeraphat-Sorasetsakul@users.noreply.github.com>
2023-03-15 18:26:30 +0700

    refactor(chart): migrate theme config to plugin (#617)
    * refactor(chart): migrate theme config to plugin
    
    * refactor(chart): rewrite set color at datasets
    
    * refactor(chart): refactor legend to use default generate legend from chartjs and refactor generateColor
    
    * refactor(chart): remove legend.ts and generate legend from chartjs instead
    
    * refactor(chart): fix typo and do clean code
    
    * refactor(chart): fix merge to receive type instead of assert as unknown
    
    * refactor(chart): do clean code
    
    * docs(chart): fix typo

--- packages/elements/src/chart/helpers/index.ts
+++ packages/elements/src/chart/helpers/index.ts
@@ -1,2 +1,1 @@
-// export * from './legend.js';
 export * from './merge.js';
--- packages/elements/src/chart/index.ts
+++ packages/elements/src/chart/index.ts
@@ -10,19 +10,28 @@ import { customElement } from '@refinitiv-ui/core/decorators/custom-element.js';
 import { property } from '@refinitiv-ui/core/decorators/property.js';
 import { query } from '@refinitiv-ui/core/decorators/query.js';
 import { VERSION } from '../version.js';
+import { color as parseColor } from '@refinitiv-ui/utils/color.js';
 
 import {
   merge,
   MergeObject
 } from './helpers/index.js';
+import type {
+  DatasetColors
+} from './helpers/types';
 
 import {
-  Chart as ChartJS
+  Chart as ChartJS,
+  Plugin,
+  ChartType,
+  ChartDataset,
+  Color,
+  LineControllerDatasetOptions
 // TODO: import only common types and let user registers specific type
 // eslint-disable-next-line import/extensions
 } from 'chart.js/auto';
 
-import type { ChartConfiguration, ChartOptions, UpdateMode } from 'chart.js';
+import type { ChartConfiguration, ChartOptions, UpdateMode, LegendItem } from 'chart.js';
 
 import type { Header } from '../header';
 import '../header/index.js';
@@ -31,6 +40,17 @@ import '../header/index.js';
 export * from 'chart.js';
 
 const CSS_COLOR_PREFIX = '--chart-color-';
+
+/* Make ChartJS to know our plugin
+ * https://www.chartjs.org/docs/latest/developers/plugins.html#typescript-typings
+ */
+declare module 'chart.js' {
+  interface PluginOptionsByType<TType extends ChartType> {
+    'ef-chart'?: {
+      enable: boolean
+    }
+  }
+}
 
 /**
  * Charting component that use ChartJS library
@@ -82,6 +102,9 @@ export class Chart extends BasicElement {
       plugins: {
         title: {
           display: false
+        },
+        'ef-chart': {
+          enable: true
         }
       }
     };
@@ -103,7 +126,7 @@ export class Chart extends BasicElement {
   }
 
   /**
-   * Safely returns the chart title
+   * Returns the chart title
    * @returns chart title
    */
   protected get chartTitle (): string {
@@ -114,6 +137,14 @@ export class Chart extends BasicElement {
     }
 
     return '';
+  }
+
+  /**
+   * Returns a dataset array
+   * @returns dataset array
+   */
+  protected get datasets (): ChartDataset[] {
+    return this.config?.data?.datasets || [];
   }
 
   /**
@@ -134,6 +165,7 @@ export class Chart extends BasicElement {
    */
   public connectedCallback (): void {
     super.connectedCallback();
+    this.setGlobalConfig();
     if (this.canvas) {
       this.createChart();
     }
@@ -149,6 +181,83 @@ export class Chart extends BasicElement {
   }
 
   /**
+   * Create plugin to set our theme into chartjs lifecycle
+   * @returns {Plugin} plugin
+   */
+  private createPlugin (): Plugin {
+    return {
+      id: 'ef-chart',
+      beforeInit: (chart: ChartJS) => {
+        const option: ChartOptions = this.themableChartOption;
+        merge(chart.config.options as unknown as MergeObject, option, true);
+      },
+      beforeUpdate: this.decorateColors
+    };
+  }
+
+  /**
+   * Themable parts of the config.
+   * This will be merged into the configuration object.
+   * @returns {ChartOptions} chart config with theme
+   */
+  protected get themableChartOption (): ChartOptions {
+    return {
+      animation: {
+        duration: this.cssVarAsNumber('--animation-duration', '0')
+      },
+      elements: {
+        line: {
+          borderWidth: this.cssVarAsNumber('--line-width', '1'),
+          tension: this.cssVarAsNumber('--line-tension', '0.5')
+        }
+      },
+      plugins: {
+        tooltip: {
+          backgroundColor: this.getComputedVariable('--tooltip-background-color', 'transparent'),
+          titleColor: this.getComputedVariable('--tooltip-title-color', 'transparent'),
+          bodyColor: this.getComputedVariable('--tooltip-body-color', 'transparent'),
+          cornerRadius: this.cssVarAsNumber('--tooltip-border-radius', '0'),
+          caretSize: this.cssVarAsNumber('--tooltip-caret-size', '0'),
+          padding: {
+            x: this.cssVarAsNumber('--tooltip-padding-x', '--tooltip-padding', '0'),
+            y: this.cssVarAsNumber('--tooltip-padding-y', '--tooltip-padding', '0')
+          },
+          titleSpacing: this.cssVarAsNumber('--tooltip-title-spacing', '0'),
+          displayColors: false
+        },
+        legend: {
+          position: ['pie', 'doughnut'].includes(this.config?.type || '') ? 'right' : 'top',
+          labels: {
+            boxWidth: this.cssVarAsNumber('--legend-key-box-width', '10'),
+            generateLabels: this.generateLegendLabels
+          }
+        }
+      }
+    };
+  }
+
+  /**
+   * Set global configuration of ChartJS
+   * @returns {void}
+   */
+  // TODO: Try and remove the need for global object modification.
+  // It's easier to cover all areas by modifying the global object,
+  // however, if possible, we should look to try and just modify local configs.
+  private setGlobalConfig (): void {
+    const cssStyle = getComputedStyle(this);
+
+    // Set font globals
+    ChartJS.defaults.color = cssStyle.getPropertyValue('color');
+    ChartJS.defaults.font.family = cssStyle.getPropertyValue('font-family');
+    ChartJS.defaults.font.size = Number(cssStyle.getPropertyValue('font-size').replace('px', ''));
+    ChartJS.defaults.font.style = cssStyle.getPropertyValue('font-style') as 'normal' | 'italic' | 'oblique' | 'initial' | 'inherit' | undefined;
+    // Set global grid color
+    ChartJS.defaults.scale.grid.color = (line) => {
+      return line.index === 0 ? this.getComputedVariable('--zero-line-color', 'transparent') : this.getComputedVariable('--grid-line-color', 'transparent');
+    };
+  }
+
+  /**
    * Handles a change of configuration object.
    * This does not fire when a property of the config object changes,
    * for this use this.updateChart() to apply changes.
@@ -161,6 +270,157 @@ export class Chart extends BasicElement {
   }
 
   /**
+   * Get as CSS variable and tries to convert it into a usable number
+   * @returns {(number|undefined)} The value as a number, or, undefined if NaN.
+   */
+  protected cssVarAsNumber (...args: string[]): number | undefined {
+    const result = Number(this.getComputedVariable(...args).replace(/\D+$/, ''));
+    return isNaN(result) ? undefined : result;
+  }
+
+  /**
+   * Inject theme color into each datasets
+   * @param {ChartJS} chart Chart.js instance
+   * @returns {void}
+   */
+  protected decorateColors = (chart: ChartJS): void => {
+    chart.config.data.datasets.forEach((dataset, datasetIndex) => {
+      let colors;
+      let borderColor;
+      let backgroundColor;
+      const isMultipleDatasets = (chart.config.data.datasets.length > 1);
+      // From old requirement, Only line, radar, scatter, polarArea type are opaque backgroundColor
+      switch (dataset.type || this.config?.type) {
+        case 'line':
+        case 'radar':
+        case 'scatter':
+          colors = this.generateColors(false, 1, datasetIndex);
+          if (!dataset.borderColor) {
+            dataset.borderColor = colors.solid;
+          }
+          if (!dataset.backgroundColor) {
+            dataset.backgroundColor = colors.opaque;
+          }
+          if (!(dataset as LineControllerDatasetOptions).pointBackgroundColor) {
+            (dataset as LineControllerDatasetOptions).pointBackgroundColor = colors.solid;
+          }
+          if (!(dataset as LineControllerDatasetOptions).pointBorderColor) {
+            (dataset as LineControllerDatasetOptions).pointBorderColor = colors.solid;
+          }
+          break;
+
+        // These types, Colors are set to an array.
+        case 'doughnut':
+        case 'pie':
+        case 'polarArea':
+          const index = isMultipleDatasets ? 0 : datasetIndex;
+          colors = this.generateColors(true, dataset.data ? dataset.data.length : 1, index);
+          borderColor = isMultipleDatasets ? this.getComputedVariable('--multi-dataset-border-color', '#fff') : colors.solid;
+          backgroundColor = this.config?.type === 'polarArea' ? colors.opaque : colors.solid;
+          if (!dataset.borderColor) {
+            dataset.borderColor = borderColor;
+          }
+          if (!dataset.backgroundColor) {
+            dataset.backgroundColor = backgroundColor;
+          }
+          // Add more colors if items aren't enough
+          if (Array.isArray(dataset.borderColor) && Array.isArray(borderColor) && dataset.borderColor.length < borderColor.length) {
+            merge(dataset.borderColor, borderColor);
+          }
+          if (Array.isArray(dataset.backgroundColor) && Array.isArray(backgroundColor) && dataset.backgroundColor.length < backgroundColor.length) {
+            merge(dataset.backgroundColor, backgroundColor);
+          }
+          break;
+
+        // These types, Colors could be string or array
+        case 'bar':
+        case 'bubble':
+          colors = this.generateColors(!isMultipleDatasets, !isMultipleDatasets && dataset.data ? dataset.data.length : 1, datasetIndex);
+          borderColor = colors.solid;
+          backgroundColor = colors.solid;
+          if (!dataset.borderColor) {
+            dataset.borderColor = colors.solid;
+          }
+          if (!dataset.backgroundColor) {
+            dataset.backgroundColor = colors.solid;
+          }
+          // Add more colors if items aren't enough
+          if (Array.isArray(dataset.borderColor) && Array.isArray(borderColor) && dataset.borderColor.length < borderColor.length) {
+            merge(dataset.borderColor, borderColor);
+          }
+
+          if (Array.isArray(dataset.backgroundColor) && Array.isArray(backgroundColor) && dataset.backgroundColor.length < backgroundColor.length) {
+            merge(dataset.backgroundColor, backgroundColor);
+          }
+          break;
+        // For other types
+        default:
+          colors = this.generateColors(false, dataset.data.length, datasetIndex);
+          if (!dataset.borderColor) {
+            dataset.borderColor = colors.solid;
+          }
+          if (!dataset.backgroundColor) {
+            dataset.backgroundColor = colors.opaque;
+          }
+          break;
+      }
+    });
+  };
+
+  /**
+   * Generates the legend labels on a given chart
+   * @param {ChartJS} chart Chart.js instance
+   * @returns {LegendItem[]} Array of label configurations
+   */
+  protected generateLegendLabels = (chart: ChartJS): LegendItem[] => {
+    const chartType = (chart.config as ChartConfiguration).type;
+    if (!chartType) {
+      return [];
+    }
+
+    let legends: LegendItem[] = [];
+    const datasets = chart.config.data.datasets;
+
+    if (
+      datasets.length
+      && chart?.config?.options?.plugins?.legend
+      && Array.isArray(datasets[0].backgroundColor)
+    ) {
+
+      if (ChartJS.overrides.pie.plugins.legend.labels.generateLabels) {
+        legends = ChartJS.overrides.pie.plugins.legend.labels.generateLabels(chart);
+      }
+
+      // Customize for doughnut chart change border color to background color
+      if (['pie', 'doughnut'].includes(chartType) && this.datasets.length > 1) {
+        legends.forEach((label: LegendItem)=> {
+          label.strokeStyle = label.fillStyle;
+        });
+      }
+
+      return legends;
+    }
+
+    if (ChartJS.defaults.plugins.legend.labels.generateLabels) {
+      legends = ChartJS.defaults.plugins.legend.labels.generateLabels(chart);
+    }
+    legends.forEach((legend, i) => {
+      legend.lineWidth = Number(datasets[i].borderWidth) || 0;
+      switch (datasets[i].type || chartType) {
+        case 'line':
+        case 'radar':
+        case 'scatter':
+          legend.fillStyle = (datasets[i] as LineControllerDatasetOptions).borderColor as Color;
+          break;
+        // For other chart types
+        default:
+          break;
+      }
+    });
+    return legends;
+  };
+
+  /**
    * Merges all the different layers of the config.
    * @returns {void}
    */
@@ -169,16 +429,38 @@ export class Chart extends BasicElement {
       return;
     }
 
-    // TODO: unknown type
-    merge(this.config as unknown as MergeObject, { options: this.requiredConfig } as MergeObject, true);
+    merge(this.config as unknown as MergeObject, { plugins: [this.createPlugin()], options: this.requiredConfig } as MergeObject, true);
   }
 
+
   /**
-   * Themes the passed-in configuration object
-   * @returns {void}
+   * Generates internal solid and opaque color set for a dataset
+   * @param {boolean} isArray Flag to return result in array or not e.g. doughnut, pie, etc
+   * @param {number} amount Amount of colors required
+   * @param {number} shift Positional shift of the color start point
+   * @returns {DatasetColors} Solid and opaque color values
    */
-  protected decorateConfig (): void {
-    this.mergeConfigs();
+  protected generateColors (isArray: boolean, amount: number, shift: number): DatasetColors {
+    const solid = [];
+    const opaque = [];
+    const alpha = Number(this.getComputedVariable('--fill-opacity', '0.2'));
+
+    amount = isArray ? amount : 1;
+
+    for (let i = shift; i < amount + shift; i++) {
+      const color = this.colors[i % this.colors.length];
+      solid.push(color);
+
+      const opaqueColor = parseColor(color);
+      if (opaqueColor) {
+        opaqueColor.opacity = alpha;
+        opaque.push(opaqueColor.toString());
+      }
+    }
+    return {
+      solid: isArray ? solid : solid[0],
+      opaque: isArray ? opaque : opaque[0]
+    };
   }
 
   /**
@@ -204,7 +486,7 @@ export class Chart extends BasicElement {
     const ctx = this.canvas.getContext('2d');
     if (ctx && this.config) {
       this.destroyChart();
-      this.decorateConfig();
+      this.mergeConfigs();
       this.manageTitle();
 
       this.chart = new ChartJS(this.canvas, this.config);
@@ -237,7 +519,7 @@ export class Chart extends BasicElement {
 
     // Stop any chart.js animations
     this.chart.stop();
-    this.decorateConfig();
+    this.mergeConfigs();
     this.manageTitle();
 
     // Update the chart
