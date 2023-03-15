31f7e8e3474df89bf97ea291de523c09aa47025e
napatbualoy <napat.bualoy2@lseg.com>
2023-03-15 15:13:18 +0700

    test(sub-label): add unit test

---
+++ packages/components/src/sub-label/__test__/__snapshots__/sub-label.test.snap.js
@@ -0,0 +1,9 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["DOM Structure Should have correct DOM structure"] = 
+`<slot>
+</slot>
+`;
+/* end snapshot DOM Structure Should have correct DOM structure */
+
