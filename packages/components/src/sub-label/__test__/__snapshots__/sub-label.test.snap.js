447adeb94f4b7175120ac863f802c119af8cd12c
Napat Bualoy <napat.bualoy@lseg.com>
2023-03-20 10:14:00 +0700

    test(sub-label): update unit test

---
+++ packages/components/src/sub-label/__test__/__snapshots__/sub-label.test.snap.js
@@ -0,0 +1,24 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["ds-sub-label DOM Structure default DOM is correct"] = 
+`<ds-sub-label>
+  Label
+</ds-sub-label>
+`;
+/* end snapshot ds-sub-label DOM Structure default DOM is correct */
+
+snapshots["ds-sub-label DOM Structure error DOM is correct"] = 
+`<ds-sub-label error="">
+  Label
+</ds-sub-label>
+`;
+/* end snapshot ds-sub-label DOM Structure error DOM is correct */
+
+snapshots["ds-sub-label DOM Structure warning DOM is correct"] = 
+`<ds-sub-label warning="">
+  Label
+</ds-sub-label>
+`;
+/* end snapshot ds-sub-label DOM Structure warning DOM is correct */
+
--- packages/components/src/sub-label/__test__/sub-label.test.js
+++ packages/components/src/sub-label/__test__/sub-label.test.js
@@ -1,4 +1,58 @@
-it('test sub-label', () => {
-  console.log('test sub label worked');
+import { fixture, expect, elementUpdated } from '@refinitiv-ui/test-helpers';
+import '@refinitiv-ui/elements/sub-label';
+
+describe('ds-sub-label', () => {
+
+  describe('DOM Structure', () => {
+      it('default DOM is correct', async () => {
+        const el = await fixture(`<ds-sub-label>Label</ds-sub-label`);
+        await expect(el).to.equalSnapshot();
+      });
+      it('error DOM is correct', async () => {
+        const el = await fixture(`<ds-sub-label error>Label</ds-sub-label`);
+        await expect(el).to.equalSnapshot();
+      });
+      it('warning DOM is correct', async () => {
+        const el = await fixture(`<ds-sub-label warning>Label</ds-sub-label`);
+        await expect(el).to.equalSnapshot();
+      });
+  });
+
+  describe('Attributes', () => {
+    describe('error', () => {
+      it('error attribute should not be presented by default', async () => {
+        const el = await fixture(`<ds-sub-label>Label</ds-sub-label`);
+        await expect(el.hasAttribute('error')).to.be.false;
+      });
+      it('error attribute should be reflected with property', async () => {
+        const el = await fixture(`<ds-sub-label error>Label</ds-sub-label`);
+        await expect(el.error).to.be.true;
+      });
+      it('error attribute should be reflected when property value has change', async () => {
+        const el = await fixture(`<ds-sub-label error>Label</ds-sub-label`);
+        el.error = false;
+
+        await elementUpdated(el);
+        await expect(el.hasAttribute('error')).to.be.false;
+      });
+    });
+    describe('warning', () => {
+      it('warning attribute should not be presented by default', async () => {
+        const el = await fixture(`<ds-sub-label>Label</ds-sub-label`);
+        await expect(el.hasAttribute('warning')).to.be.false;
+      });
+      it('warning attribute should be reflected with property', async () => {
+        const el = await fixture(`<ds-sub-label warning>Label</ds-sub-label`);
+        await expect(el.warning).to.be.true;
+      });
+      it('warning attribute should be reflected when property value has change', async () => {
+        const el = await fixture(`<ds-sub-label warning>Label</ds-sub-label`);
+        el.warning = false;
+
+        await elementUpdated(el);
+        await expect(el.hasAttribute('warning')).to.be.false;
+      });
+    });
+  });
 });
 
