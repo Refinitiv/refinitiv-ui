8602c4ceb3fb52ca623f17a612d331707ec32948
Napat Bualoy <napat.bualoy@lseg.com>
2023-03-20 10:15:27 +0700

    test(sub-checkbox): update unit test

--- packages/components/src/sub-checkbox/__test__/__snapshots__/sub-checkbox.test.snap.js
+++ packages/components/src/sub-checkbox/__test__/__snapshots__/sub-checkbox.test.snap.js
@@ -1,22 +1,22 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["ds-sub-checkbox DOM Structure DOM default is correct"] = 
+snapshots["ds-sub-checkbox DOM Structure default DOM is correct"] = 
 `<ds-sub-checkbox tabindex="0">
 </ds-sub-checkbox>
 `;
-/* end snapshot ds-sub-checkbox DOM Structure DOM default is correct */
+/* end snapshot ds-sub-checkbox DOM Structure default DOM is correct */
 
-snapshots["ds-sub-checkbox DOM Structure DOM checked is correct"] = 
+snapshots["ds-sub-checkbox DOM Structure checked DOM is correct"] = 
 `<ds-sub-checkbox
   checked=""
   tabindex="0"
 >
 </ds-sub-checkbox>
 `;
-/* end snapshot ds-sub-checkbox DOM Structure DOM checked is correct */
+/* end snapshot ds-sub-checkbox DOM Structure checked DOM is correct */
 
-snapshots["ds-sub-checkbox DOM Structure DOM disabled unchecked is correct"] = 
+snapshots["ds-sub-checkbox DOM Structure disabled unchecked DOM are correct"] = 
 `<ds-sub-checkbox
   aria-disabled="true"
   disabled=""
@@ -25,18 +25,18 @@ snapshots["ds-sub-checkbox DOM Structure DOM disabled unchecked is correct"] =
 >
 </ds-sub-checkbox>
 `;
-/* end snapshot ds-sub-checkbox DOM Structure DOM disabled unchecked is correct */
+/* end snapshot ds-sub-checkbox DOM Structure disabled unchecked DOM are correct */
 
-snapshots["ds-sub-checkbox DOM Structure DOM readonly unchecked is correct"] = 
+snapshots["ds-sub-checkbox DOM Structure readonly unchecked DOM are correct"] = 
 `<ds-sub-checkbox
   readonly=""
   tabindex="0"
 >
 </ds-sub-checkbox>
 `;
-/* end snapshot ds-sub-checkbox DOM Structure DOM readonly unchecked is correct */
+/* end snapshot ds-sub-checkbox DOM Structure readonly unchecked DOM are correct */
 
-snapshots["ds-sub-checkbox DOM Structure DOM disabled checked is correct"] = 
+snapshots["ds-sub-checkbox DOM Structure disabled checked DOM are correct"] = 
 `<ds-sub-checkbox
   aria-disabled="true"
   checked=""
@@ -46,9 +46,9 @@ snapshots["ds-sub-checkbox DOM Structure DOM disabled checked is correct"] =
 >
 </ds-sub-checkbox>
 `;
-/* end snapshot ds-sub-checkbox DOM Structure DOM disabled checked is correct */
+/* end snapshot ds-sub-checkbox DOM Structure disabled checked DOM are correct */
 
-snapshots["ds-sub-checkbox DOM Structure DOM readonly checked is correct"] = 
+snapshots["ds-sub-checkbox DOM Structure readonly checked DOM are correct"] = 
 `<ds-sub-checkbox
   checked=""
   readonly=""
@@ -56,5 +56,5 @@ snapshots["ds-sub-checkbox DOM Structure DOM readonly checked is correct"] =
 >
 </ds-sub-checkbox>
 `;
-/* end snapshot ds-sub-checkbox DOM Structure DOM readonly checked is correct */
+/* end snapshot ds-sub-checkbox DOM Structure readonly checked DOM are correct */
 
