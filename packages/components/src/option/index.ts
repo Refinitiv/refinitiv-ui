10f86f2eab47f7136a030a809ccf5ac78c137c12
Sarin Udompanish <sarin.udompanish2@lseg.com>
2023-03-21 11:53:31 +0700

    feat: add option component

---
+++ packages/components/src/option/index.ts
@@ -0,0 +1,81 @@
+import { html, css, CSSResultGroup, TemplateResult, ControlElement } from '@refinitiv-ui/core';
+import { property } from '@refinitiv-ui/core/decorators/property.js';
+import { customElement } from '@refinitiv-ui/core/decorators/custom-element.js';
+import { VERSION } from '../version.js';
+
+/**
+ * An option for select
+ */
+@customElement('ds-option', { theme: false })
+export class Option extends ControlElement {
+  protected readonly defaultRole = 'option';
+
+  /**
+   * Element version number
+   * @returns version number
+   */
+  static get version (): string {
+    return VERSION;
+  }
+
+  /**
+   * Highlight the option
+   */
+  @property({ type: Boolean, reflect: true })
+  public highlighted = false;
+
+  /**
+   * Indicates that the option is selected
+   */
+  @property({ type: Boolean, reflect: true })
+  public selected = false;
+
+  /**
+   * A `CSSResultGroup` that will be used to style the host,
+   * slotted children and the internal template of the element.
+   * @returns CSS template
+   */
+  static get styles (): CSSResultGroup {
+    return css`
+      :host {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        box-sizing: border-box;
+        outline: none;
+        padding: var(--ds-space-xxx-small) var(--ds-space-x-small);
+        min-height: var(--ds-control-height);
+        color: var(--ds-control-color);
+        background-color: var(--ds-control-background-color);
+        border: var(--ds-control-border-width) var(--ds-control-border-style) transparent;
+        border-radius: var(--ds-control-border-radius);
+      }
+      :host([selected]) {
+        color: var(--ds-control-color);
+      }
+      :host(:focus),
+      :host([highlighted]) {
+        color: var(--ds-control-focus-color);
+        border-color: var(--ds-control-focus-border-color);
+        background-color: var(--ds-control-focus-background-color);
+      }
+    `;
+  }
+
+  /**
+   * A `TemplateResult` that will be used
+   * to render the updated internal template.
+   * @return {TemplateResult}  Render template
+   */
+  protected render (): TemplateResult {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ds-option': Option;
+  }
+}
