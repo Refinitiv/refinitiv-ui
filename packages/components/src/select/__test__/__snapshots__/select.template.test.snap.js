07ff7af72fb4b860b1584c18c8f2d1cb836cd5a6
Sarin Udompanish <sarin.udompanish2@lseg.com>
2023-03-27 18:24:48 +0700

    feat: add select element

---
+++ packages/components/src/select/__test__/__snapshots__/select.template.test.snap.js
@@ -0,0 +1,128 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["select/Template Template Parts Empty DOM has all required parts"] = 
+`<ui-sub-select
+  aria-expanded="false"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Empty DOM has all required parts */
+
+snapshots["select/Template Template Parts Lazy Render: options"] = 
+`<ui-sub-select
+  aria-expanded="false"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Lazy Render: options */
+
+snapshots["select/Template Template Parts Lazy Render: data"] = 
+`<ui-sub-select
+  aria-expanded="false"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Lazy Render: data */
+
+snapshots["select/Template Template Parts Data is reflected to render"] = 
+`<ui-sub-select
+  aria-expanded="true"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  opened=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Data is reflected to render */
+
+snapshots["select/Template Template Parts Data is reflected to reverse render"] = 
+`<ui-sub-select
+  aria-expanded="true"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  opened=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Data is reflected to reverse render */
+
+snapshots["select/Template Template Parts Data is reflected to render null data"] = 
+`<ui-sub-select
+  aria-expanded="true"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  opened=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Data is reflected to render null data */
+
+snapshots["select/Template Template Parts Lazy Render: options opened"] = 
+`<ui-sub-select
+  aria-expanded="true"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  opened=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Lazy Render: options opened */
+
+snapshots["select/Template Template Parts Lazy Render: data opened"] = 
+`<ui-sub-select
+  aria-expanded="true"
+  aria-haspopup="listbox"
+  aria-labelledby=""
+  opened=""
+  part="select"
+  role="button"
+  tabindex="0"
+>
+  <slot>
+  </slot>
+</ui-sub-select>
+`;
+/* end snapshot select/Template Template Parts Lazy Render: data opened */
+
