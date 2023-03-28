14d6e362ed472a5ab65ade0d87106bab0914207f
napatbualoy <napat.bualoy2@lseg.com>
2023-03-28 14:10:57 +0700

    test(sub-footer): update snapshot

---
+++ packages/components/src/sub-footer/__test__/__snapshots__/sub-footer.test.snap.js
@@ -0,0 +1,18 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["ui-sub-footer DOM Structure Default DOM is correct"] = 
+`<ui-sub-footer>
+</ui-sub-footer>
+`;
+/* end snapshot ui-sub-footer DOM Structure Default DOM is correct */
+
+snapshots["ui-sub-footer DOM Structure DOM with slot content is correct"] = 
+`<ui-sub-footer>
+  <div>
+    Content
+  </div>
+</ui-sub-footer>
+`;
+/* end snapshot ui-sub-footer DOM Structure DOM with slot content is correct */
+
