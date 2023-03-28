419e589b60cefa7f261f57a166f25a1623a3d8d6
Tremayne Christ <trem.christ@lseg.com>
2023-03-28 22:22:22 +0100

    feat(demo): update demo page to show form submission

--- demo/app.ts
+++ demo/app.ts
@@ -1,2 +1,7 @@
 import '@refinitiv-ui/components/template-login.js';
-import '@refinitiv-ui/components/checkbox.js';
+import '@refinitiv-ui/components/select.js';
+
+const el = document.getElementById('sign-in');
+
+// eslint-disable-next-line
+el?.addEventListener('submit', (event) => console.log('Submit', (event as unknown as CustomEvent).detail));
