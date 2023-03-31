aca4776d0634070e8cd3fb44062eb16e4f2caafe
wsuwt <wasuwat.limsuparhat2@lseg.com>
2023-03-31 15:11:23 +0700

    fix: label color

--- packages/components/src/sub-label/index.ts
+++ packages/components/src/sub-label/index.ts
@@ -23,30 +23,11 @@ export class SubLabel extends BasicElement {
     return css`
       :host {
         display: inline-block;
-        color: var(--ds-text-body-color);
-        font-size: var(--ds-text-body-size);
-      }
-      :host([error]),
-      :host([error][warning]) {
-        color: var(--ds-control-error-color);
-      }
-      :host([warning]) {
-        color: var(--ds-control-warning-color);
+        color: var(--control-content-primary);
+        font: var(--code-only-control-label-default);
       }
     `;
   }
-
-  /**
-   * Set state to error
-   */
-  @property({ type: Boolean, reflect: true })
-  public error = false;
-
-  /**
-   * Set state to warning
-   */
-  @property({ type: Boolean, reflect: true })
-  public warning = false;
 
   /**
    * A `TemplateResult` that will be used
