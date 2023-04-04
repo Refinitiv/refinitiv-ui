99be3756dfa51daf0adbc374191d3ae5b3ff9f7c
wsuwt <wasuwat.limsuparhat2@lseg.com>
2023-04-04 19:14:18 +0700

    feat: add responsive footer

--- packages/components/src/sub-footer/index.ts
+++ packages/components/src/sub-footer/index.ts
@@ -27,6 +27,7 @@ export class SubFooter extends BasicElement {
     return css`
       :host {
         display: flex;
+        flex-wrap: wrap;
         align-items: center;
         box-sizing: border-box;
         width: 100%;
@@ -35,22 +36,67 @@ export class SubFooter extends BasicElement {
         font: var(--link-md-emphasis-all);
         background-color: var(--base-bg-emphasis);
       }
-      ::slotted(*) {
-        font: var(--copy-md-emphasis-all);
-        color: var(--base-content-primary-on-invert);
-      }
       ::slotted(a) {
-        text-underline-offset: var(--width-fixed3);
-        text-decoration-thickness: var(--width-fixed1)
+        position: relative;
+        outline-offset: 0;
+        box-sizing: border-box;
+
+        font: var(--link-md-emphasis-all);
+        color: var(--link-content-primary-on-invert);
+        border: var(--width-fixed2) solid transparent;
+
+        text-underline-offset: var(--width-fixed2);
+        text-decoration-thickness: var(--width-fixed1);
+      }
+      ::slotted(a:not(:last-child))::after {
+        content: '';
+        position: absolute;
+        top: 4px;
+        left: 0px;
+
+        width: calc(var(--footer-padding-between) + 100%);
+        height: 12px;
+        border-right: var(--width-fixed2) solid var(--link-content-primary-on-invert);
+      }
+      ::slotted(a:visited) {
+        color: var(--link-content-visited-on-invert);
+      }
+      ::slotted(a:hover) {
+        color: var(--link-content-hover-on-invert);
+        text-decoration-thickness: var(--width-fixed2);
       }
       ::slotted(a:focus-visible) {
-        outline: var(--control-focused-ring-on-invert);
+        color: var(--link-content-focused-on-invert);
+        border: var(--control-focused-ring-on-invert);
+        text-decoration-thickness: var(--width-fixed2);
+        outline: none;
       }
       ::slotted(a:not(:last-child)) {
         margin-right: var(--footer-padding-between);
       }
       ::slotted(a:not(:first-child)) {
         margin-left: var(--footer-padding-between);
+      }
+      @media screen and (max-width: 639px) {
+        :host {
+          height: var(--footer-xs-height);
+          padding: var(--footer-xs-padding-vertical) var(--footer-xs-padding-horizontal);
+        }
+        ::slotted(a) {
+          text-align: center;
+          /* min-width: var(--footer-xs-link-width); */
+        }
+        ::slotted(a:not(:last-child):not(:first-child)) {
+          margin-right: 0;
+        }
+        ::slotted(a:nth-child(2))::after {
+          display: none;
+        }
+        ::slotted(a:last-child) {
+          margin-left: 0;
+          margin-right: 100%;
+          min-width: max-content;
+        }
       }
     `;
   }
