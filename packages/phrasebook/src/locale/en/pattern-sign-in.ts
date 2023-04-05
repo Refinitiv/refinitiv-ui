1033ceed25cbd10bcb2bd98e083fe14b073231c5
wsuwt <wasuwat.limsuparhat2@lseg.com>
2023-04-05 16:42:10 +0700

    feat: add lang switching support

---
+++ packages/phrasebook/src/locale/en/pattern-sign-in.ts
@@ -0,0 +1,10 @@
+// Component docs https://elf.int.refinitiv.com/elements/pagination.html
+import { Phrasebook } from '../../translation.js';
+import './shared.js';
+
+const translations = {
+};
+
+Phrasebook.define('en', 'ui-pattern-sign-in', translations);
+
+export default translations;
