2737a4a9e254aa5d2c0408eac6fb9ce65d0a3611
Sakchai Homhual <Sakchai.Homhual@refinitiv.com>
2023-05-02 15:19:03 +0700

    build: update browsers config for unit testing

--- browsers.config.mjs
+++ browsers.config.mjs
@@ -7,8 +7,8 @@ const BrowserStack = {
   supportedBrowsers: [
     'chrome', 'chrome_minus1',
     'firefox', 'firefox_minus1', 'firefox_minus2',
-    'ios', 'ios_minus1',
-    'android', 'android_minus1','android_minus2'
+    'ios',
+    'android', 'android_minus1',
   ],
   availableBrowsers: [
     'default', // default browsers alias
@@ -21,8 +21,11 @@ const BrowserStack = {
     // Safari are moved out from default and supported temporary because BrowserStack connection issue
     'safari', 'safari_minus1', 'safari_minus2',
 
-    // IOS 14 has the problem with issue undefined ResizeObserver is undefined
-    'ios_minus2'
+    // iOS 14, 15 has the problem with issue undefined ResizeObserver is undefined
+    'ios_minus1', 'ios_minus2',
+
+    // Android 11 hast the issue render date format in calendar
+    'android_minus2'
   ]
 };
 // combine default, supported & aliases into complete available browsers
