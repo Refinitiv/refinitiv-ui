2114ba686cd86b155debb6cc6e5f1d2e730ae96a
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-05-09 14:07:35 +0700

    feat(test-helper): add mocha util converting resize observer loop error to warning

---
+++ packages/test-helpers/src/resize-observer-loop-handler.ts
@@ -0,0 +1,20 @@
+// ResizeObserver loop error is considered benign as discussed in https://github.com/w3c/csswg-drafts/issues/5023
+// This module converts the error into a warning instead
+// Note that, mocha must be available in an imported context
+before(() => {
+  const originalOnError = window.onerror;
+  window.onerror = function (event, ...args) {
+    // Firefox: `ResizeObserver loop completed with undelivered notifications.`
+    // Chrome: `ResizeObserver loop limit exceeded`
+    // Safari: `ResizeObserver loop completed with undelivered notifications`
+    // Each browser logs a slightly different messages yet they all start with `ResizeObserver loop`
+    if (typeof event === 'string' && event.startsWith('ResizeObserver loop')) {
+      // eslint-disable-next-line no-console
+      console.warn(`warning: ${event}`);
+      return true;
+    }
+    else {
+      return originalOnError ? originalOnError(event, ...args) as boolean : false;
+    }
+  };
+});
