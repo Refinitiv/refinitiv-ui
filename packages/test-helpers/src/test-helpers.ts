dbdbcd3e5214e1ec838b3beac0881e84fa8344fb
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-05-10 16:51:49 +0700

    refactor(test-helpers): refactor resize observer loop handler to test helper's side effect

--- packages/test-helpers/src/test-helpers.ts
+++ packages/test-helpers/src/test-helpers.ts
@@ -94,3 +94,25 @@ export const isNear = (a: number, b: number, distance: number, inclusive = true)
  * @returns string
  */
 export const replaceWhitespace = (text: string): string => text.replace(/\s/g, ' ');
+
+// ResizeObserver loop error is considered benign as discussed in https://github.com/w3c/csswg-drafts/issues/5023
+// This module converts the error into a warning instead
+// Note that, mocha must be available in an imported context
+before(function () {
+  const originalOnError = window.onerror;
+  window.onerror = function (event, ...args) {
+    // Firefox: `ResizeObserver loop completed with undelivered notifications.`
+    // Chrome: `ResizeObserver loop limit exceeded`
+    // Safari: `ResizeObserver loop completed with undelivered notifications`
+    // Each browser logs a slightly different messages yet they all start with `ResizeObserver loop`
+    /* istanbul ignore else */
+    if (typeof event === 'string' && event.startsWith('ResizeObserver loop')) {
+      // eslint-disable-next-line no-console
+      console.warn(`warning: ${event}`);
+      return true;
+    }
+    else {
+      return originalOnError ? originalOnError(event, ...args) as boolean : false;
+    }
+  };
+});
