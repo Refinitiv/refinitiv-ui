415e36cecf752c52bc762df33f4b6461c4eb00a5
Sarin-Udompanish <sarin.udompanish2@lseg.com>
2023-05-10 15:13:52 +0700

    chore: remove all web-dev-server config and update demo

--- packages/demo-block/src/demo-block.ts
+++ packages/demo-block/src/demo-block.ts
@@ -9,6 +9,89 @@ import {
 import { customElement } from '@refinitiv-ui/core/decorators/custom-element.js';
 import { property } from '@refinitiv-ui/core/decorators/property.js';
 
+const Themes = [
+  'elemental/light',
+  'elemental/dark',
+  'halo/light',
+  'halo/dark',
+  'solar/pearl',
+  'solar/charcoal'
+];
+
+const url = new URL(window.location as unknown as string);
+
+const getCurrentTheme = (): string => {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  return `${url.searchParams.get('theme')}/${url.searchParams.get('variant')}` || Themes[0];
+};
+
+const getNextTheme = (): string => {
+  return Themes[(Themes.indexOf(getCurrentTheme()) + 1) % Themes.length];
+};
+
+const getPrevTheme = (): string => {
+  return Themes[(Themes.indexOf(getCurrentTheme()) + Themes.length - 1) % Themes.length];
+};
+
+/* istanbul ignore next */
+const changeTheme = (theme: string): void => {
+  const searchParams = theme.split('/');
+  url.searchParams.set('theme', searchParams[0]);
+  url.searchParams.set('variant', searchParams[1]);
+  window.location.search = url.searchParams.toString();
+};
+
+const removePrefixName = (name: string): string => {
+  return name.replace('elf-theme-', '');
+};
+
+const currentTheme = getCurrentTheme();
+
+// Next Theme
+const nextBtn = document.createElement('span');
+nextBtn.id = 'change-theme';
+nextBtn.setAttribute('title', getNextTheme());
+nextBtn.style.backgroundColor = '#f5f5f6';
+nextBtn.style.position = 'fixed';
+nextBtn.style.right = '10px';
+nextBtn.style.top = '10px';
+nextBtn.style.fontSize = '0';
+nextBtn.style.cursor = 'pointer';
+nextBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>';
+nextBtn.addEventListener('click', () => {
+  changeTheme(getNextTheme());
+});
+
+
+// Prev Theme
+const prevBtn = nextBtn.cloneNode(true) as HTMLElement;
+prevBtn.id = 'prev_theme_btn';
+prevBtn.setAttribute('title', getPrevTheme());
+prevBtn.style.right = '40px';
+prevBtn.style.transform = 'rotate(180deg)';
+prevBtn.addEventListener('click', () => {
+  changeTheme(getPrevTheme());
+});
+
+// Label Theme
+const themeLabel = document.createElement('span');
+themeLabel.id = 'current_theme_btn';
+themeLabel.setAttribute('title', currentTheme);
+themeLabel.style.backgroundColor = '#f5f5f6';
+themeLabel.style.color = '#3e444f';
+themeLabel.style.position = 'fixed';
+themeLabel.style.fontSize = '15px';
+themeLabel.style.left = '10px';
+themeLabel.style.top = '10px';
+themeLabel.style.padding = '2px 4px';
+themeLabel.style.fontWeight = '600';
+themeLabel.style.textTransform = 'uppercase';
+themeLabel.innerHTML = removePrefixName(currentTheme);
+
+document.body.appendChild(nextBtn);
+document.body.appendChild(prevBtn);
+document.body.appendChild(themeLabel);
+
 @customElement('demo-block', { theme: false })
 export class DemoBlock extends BasicElement {
 
