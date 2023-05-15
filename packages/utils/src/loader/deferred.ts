9a85a3ea3ba96147051b450c76d218e34ed181f4
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-05-15 09:44:02 +0700

    feat(utils): add promise-state APIs to Deferred

--- packages/utils/src/loader/deferred.ts
+++ packages/utils/src/loader/deferred.ts
@@ -1,5 +1,11 @@
 type PromiseField<T> = (value: T) => void
 
+enum PromiseState {
+  pending,
+  resolved,
+  rejected
+}
+
 /**
  * Creates pending promise, that can be resolved or rejected manually
  */
@@ -12,6 +18,8 @@ export class Deferred<T> {
   private _reject: PromiseField<T> = () => {
   };
 
+  private state = PromiseState.pending;
+
   private _promise: Promise<T> = new Promise<T>((resolve, reject) => {
     this._reject = reject;
     this._resolve = resolve;
@@ -23,9 +31,27 @@ export class Deferred<T> {
 
   public resolve (value: T): void {
     this._resolve(value);
+    this.state = PromiseState.resolved;
   }
 
   public reject (value: T): void {
     this._reject(value);
+    this.state = PromiseState.rejected;
+  }
+
+  public isSettled (): boolean {
+    return this.state === PromiseState.rejected || this.state === PromiseState.resolved;
+  }
+
+  public isPending (): boolean {
+    return this.state === PromiseState.pending;
+  }
+
+  public isRejected (): boolean {
+    return this.state === PromiseState.rejected;
+  }
+
+  public isResolved (): boolean {
+    return this.state === PromiseState.resolved;
   }
 }
