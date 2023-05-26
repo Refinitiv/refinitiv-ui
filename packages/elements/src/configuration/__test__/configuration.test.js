ba5848a1d7dde26d4ba4fdbb259e32ff4180400e
nantawat.poothong <nantawat.poothong@lseg.com>
2023-05-26 17:20:22 +0700

    test(configuration): update test case

--- packages/elements/src/configuration/__test__/configuration.test.js
+++ packages/elements/src/configuration/__test__/configuration.test.js
@@ -1,16 +1,17 @@
-import { expect, fixture } from '@refinitiv-ui/test-helpers';
-import { tickSvgBase64 } from './helpers/helpers.js'
+import { expect, fixture, nextFrame } from '@refinitiv-ui/test-helpers';
+import { tickSvgBase64, tickSvg, isEqualSvg } from './helpers/helpers.js'
 import '@refinitiv-ui/elements/configuration';
 import '@refinitiv-ui/elements/icon';
 import '@refinitiv-ui/elemental-theme/light/ef-icon.js';
 
-describe('config/Icon', () => {
+describe('configuration/Configuration', () => {
   it('should pass config to icon correctly', async () => {
     const elConfig = await fixture('<ef-configuration><ef-icon></ef-icon></ef-configuration>');
-    elConfig.icon = { map: { "tick": tickSvgBase64 }}
+    elConfig.icon = { map: { "tick-base64": tickSvgBase64 }};
     const elIcon = elConfig.querySelector('ef-icon');
-    elIcon.icon = 'tick';
-    expect(elIcon.iconMap).to.equal(tickSvgBase64, 'icon map value should same as config icon value')
+    elIcon.icon = 'tick-base64';
+    await nextFrame(2);
+    const svg = elIcon.shadowRoot.querySelector('svg');
+   await expect(isEqualSvg(svg.outerHTML, tickSvg)).to.equal(true, 'Should render SVG, from the server response');
   });
 });
-
