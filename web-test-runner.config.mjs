7c11fe3f4032c97c4923bf9be52dc402cf898d59
Sakchai Homhual <Sakchai.Homhual@refinitiv.com>
2023-05-31 12:33:42 +0700

    test: set Web Test Runner use esbuild plugins

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { defaultReporter } from '@web/test-runner';
 import { ROOT, PACKAGES_ROOT } from './scripts/helpers/esm.mjs';
 import { playwrightLauncher } from '@web/test-runner-playwright';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { Buffer } from 'node:buffer';
 
 // Workaround for issue UTF-8 wide characters are unsupported
@@ -50,5 +51,8 @@ export default {
   browserStartTimeout: 600000, // 10 minutes
   testsStartTimeout: 600000, // 10 minutes
   testsFinishTimeout: 600000, // 10 minutes
-  plugins: [ pluginJsBufferToString ]
+  plugins: [
+    esbuildPlugin({ ts: true }), // Workaround: prevent the issue "Could not import module ..." (randomly)
+    pluginJsBufferToString,
+  ]
 };
