5015e332d1f6e140046fab5ba77acff0207d0608
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-06-02 10:41:23 +0700

    chore(chart): add file extension to imports from chart.js

--- packages/elements/src/chart/plugins/doughnut-center-label.ts
+++ packages/elements/src/chart/plugins/doughnut-center-label.ts
@@ -11,7 +11,7 @@ import type {
   ArcElement
 } from 'chart.js';
 // eslint-disable-next-line import/extensions
-import { getHoverColor } from 'chart.js/helpers';
+import { getHoverColor } from 'chart.js/helpers/helpers.js';
 
 interface Selectable {
   _select: ActiveElement[];
