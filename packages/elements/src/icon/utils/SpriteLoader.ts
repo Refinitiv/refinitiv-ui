e0e392e884a62080c915ca5bc63d92eb39e22fc7
nantawat.poothong <nantawat.poothong@lseg.com>
2023-06-12 17:40:48 +0700

    fix(icon): update sprite

--- packages/elements/src/icon/utils/SpriteLoader.ts
+++ packages/elements/src/icon/utils/SpriteLoader.ts
@@ -1,9 +1,19 @@
 import { SVGLoader } from '@refinitiv-ui/utils/loader.js';
 const spriteCache = new Map<string, Promise<Document>>();
+
+/**
+ * Checks a string to see if it's a valid URL
+ * @param str String to test
+ * @returns is URL
+ */
+const isUrl = (str: string): boolean => (/^(https?:\/{2}|\.?\/)/i).test(str);
 class SvgSpriteLoader extends SVGLoader {
 
   // eslint-disable-next-line
   public async getSrc (name: string): Promise<string> {
+    if (isUrl(name)) {
+      return name;
+    }
     return name ? `https://cdn.ppe.refinitiv.com/public/libs/elf/assets/elf-theme-halo/resources/sprites/icons.svg#${name}` : '';
     // return name ? `${await this.getCdnPrefix()}#${name}` : '';
   }
@@ -16,23 +26,18 @@ class SvgSpriteLoader extends SVGLoader {
     return new DOMParser().parseFromString(sprite, 'image/svg+xml');
   }
 
-  private async getIconFragment (spritePromise: Promise<Document> | undefined, iconName: string): Promise<string> {
+  private async getIconFragment (spritePromise: Promise<Document> | undefined, iconName: string): Promise<string | undefined> {
     const sprite = await spritePromise;
     const icon = sprite?.getElementById(iconName);
-    if (!icon) {
-      throw new Error("SvgSpriteLoader: couldn't detect icon");
-    }
-    else {
-      return icon.outerHTML;
-    }
+    return icon ? icon.outerHTML : undefined;
   }
 
-  public async use (src: string): Promise<string> {
-    const { hash, href } = new URL(src);
+  public async use (src: string): Promise<string | undefined> {
+    const { hash, href } = new URL(await this.getSrc(src));
     const iconName = hash.replace('#', '');
     const cacheKey = href.replace(hash, '');
     if (!iconName) {
-      throw new Error("SvgSpriteLoader: couldn't detect SVG icon name");
+      return;
     }
     if (!spriteCache.has(cacheKey)) {
       spriteCache.set(cacheKey, this.loadSprite());
@@ -41,5 +46,4 @@ class SvgSpriteLoader extends SVGLoader {
   }
 }
 const instance = new SvgSpriteLoader();
-const preload = () => instance.loadSVG('sprite/icons');
-export { instance as SvgSpriteLoader, preload };
+export { instance as SvgSpriteLoader };
