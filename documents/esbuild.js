b82387db9c9e664f6ccd65468c7eb06e9e2edf22
Wasuwat Limsuparhat <86233706+wsuwt@users.noreply.github.com>
2023-06-13 18:51:10 +0700

    refactor: use shared option

--- documents/esbuild.js
+++ documents/esbuild.js
@@ -1,35 +1,33 @@
 const fg = require('fast-glob');
 const esbuild = require('esbuild');
 
+const OPTIONS = {
+  format: 'esm',
+  bundle: true,
+  minify: true,
+  treeShaking: true,
+  logLevel: 'error',
+};
+
 const handler = async () => {
   const elements = fg('../packages/elements/lib/*/index.js');
   const themes = fg('../packages/elements/lib/*/themes/halo/*/index.js');
 
   const entryPoints = (await Promise.all([elements, themes])).flat();
 
-  const bundleOptions = {
-    format: 'esm',
-    bundle: true,
-    minify: true,
-    treeShaking: true,
-    logLevel: 'error',
-  }
-
   await esbuild.build({
     entryPoints: entryPoints,
-    outbase:'../packages/elements/lib',
+    outbase: '../packages/elements/lib',
     outdir: 'dist/resources/elements',
-    ...bundleOptions,
+    ...OPTIONS,
   });
 
   const nativeThemes = await fg('../packages/halo-theme/*/imports/native-elements.js');
   await esbuild.build({
     entryPoints: nativeThemes,
     outdir: 'dist/resources/halo-theme',
-    ...bundleOptions,
+    ...OPTIONS,
   });
 };
 
 handler();
-
-
