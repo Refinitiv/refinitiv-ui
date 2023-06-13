e9a4a0925bb347d3acd81a90c38481da3e71c9cd
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2023-06-13 10:54:38 +0700

    fix(chart): fix radar backdrop

--- packages/elements/src/chart/elements/chart.ts
+++ packages/elements/src/chart/elements/chart.ts
@@ -295,7 +295,8 @@ export class Chart extends BasicElement {
     ChartJS.defaults.scale.grid.color = (line) => {
       return line.index === 0 ? this.getComputedVariable('--zero-line-color', 'transparent') : this.getComputedVariable('--grid-line-color', 'transparent');
     };
-    if (this.config?.type === 'polarArea' || this.config?.type === 'radar') {
+
+    if (ChartJS.defaults.scales.radialLinear) {
       ChartJS.defaults.scales.radialLinear.ticks.showLabelBackdrop = false;
     }
   }
