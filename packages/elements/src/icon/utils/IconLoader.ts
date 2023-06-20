4c2e8d1cd2669b4fb17d2962b6668a3b0142a9ee
nantawat.poothong <nantawat.poothong@lseg.com>
2023-06-20 10:01:44 +0700

    fix(icon): add deprecated warning for preload method

--- packages/elements/src/icon/utils/IconLoader.ts
+++ packages/elements/src/icon/utils/IconLoader.ts
@@ -1,4 +1,5 @@
 import { SVGLoader } from '@refinitiv-ui/utils/loader.js';
+import { DeprecationNotice } from '@refinitiv-ui/core';
 
 /**
  * Caches and provides icon SVGs, Loaded either by name from CDN or directly by URL.
@@ -11,6 +12,7 @@ const iconLoaderInstance = new IconLoader();
 export { iconLoaderInstance as IconLoader };
 
 /**
+ * @deprecated Icon `preload` method is deprecated.
  * Helper function to preload set of icons.
  * It could help to reduce icon loading delay when ef-icon has a known set of icons that it can use.
  * @param attrs - list of arguments, representing icons.
@@ -18,5 +20,6 @@ export { iconLoaderInstance as IconLoader };
  * @returns Array of promises, which will be resolved with SVG bodies.
  */
 export const preload = (...attrs: string[]): Promise<string | undefined>[] => {
+  new DeprecationNotice('Icon `preload` method is deprecated.').once();
   return attrs.map(icon => iconLoaderInstance.loadSVG(icon));
 };
