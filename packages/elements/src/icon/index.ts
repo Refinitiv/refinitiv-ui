31e2eb99bc022123546d42f9a02b4ad5892bff2c
Nantawat Poothong <102957966+Nantawat-Poothong@users.noreply.github.com>
2023-06-21 17:18:51 +0700

    chore(icon): add deprecated warning for icon preload method (#760)
    * chore(utils): update JSDoc text deprecation notice
    
    ---------
    
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>

--- packages/elements/src/icon/index.ts
+++ packages/elements/src/icon/index.ts
@@ -86,7 +86,7 @@ export class Icon extends BasicElement {
   }
 
   /**
-   * Deprecation noticed, used to display a warning message
+   * Deprecation notice displays a warning message
    * when deprecated features are used.
    */
   private deprecationNotice = new DeprecationNotice('`src` attribute and property are deprecated. Use `icon` for attribute and property instead.');
