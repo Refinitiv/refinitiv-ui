950764d1e668c244f2ce6d97f79385f563940e75
Tremayne Christ <trem.christ@lseg.com>
2023-06-29 22:03:56 +0100

    chore(docs): replace v6 in pathname

--- functions/v6/[[catchall]].js
+++ functions/v6/[[catchall]].js
@@ -1,5 +1,6 @@
 export function onRequestGet({request}) {
   const url = new URL(request.url);
-  const proxyUrl = `https://v6.refinitiv-ui.pages.dev${url.pathname}`;
+  const pathname = url.pathname.replace(/^\/v6\/?/, '/');
+  const proxyUrl = `https://v6.refinitiv-ui.pages.dev${pathname}`;
   return fetch(proxyUrl);
 }
