4e990798a34c3e46cb61d1dd080aaa2f5d762e25
Tremayne Christ <trem.christ@lseg.com>
2023-06-30 10:14:13 +0100

    chore(docs): revert proxy url for prod

--- functions/v6/[[catchall]].js
+++ functions/v6/[[catchall]].js
@@ -1,6 +1,6 @@
 export function onRequestGet({request}) {
   const url = new URL(request.url);
   const pathname = url.pathname.replace(/^\/v6\/?/, '/');
-  const proxyUrl = `https://v6-docs-base-url.refinitiv-ui.pages.dev${pathname}`;
+  const proxyUrl = `https://v6.refinitiv-ui.pages.dev${pathname}`;
   return fetch(proxyUrl);
 }
