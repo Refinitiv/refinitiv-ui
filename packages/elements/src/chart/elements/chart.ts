1d63ead95582a58eb6d1f04dfca4206e6dfb1fa4
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2023-07-17 16:53:01 +0700

    fix(chart): font doesn't update after font loaded

--- packages/elements/src/chart/elements/chart.ts
+++ packages/elements/src/chart/elements/chart.ts
@@ -328,6 +328,8 @@ export class Chart extends BasicElement {
    * @returns {void}
    */
   protected decorateColors = (chart: ChartJS): void => {
+    // set global config again to prevent unloaded font in time.
+    this.setGlobalConfig();
     chart.config.data.datasets.forEach((dataset, datasetIndex) => {
       let colors;
       let borderColor;
@@ -570,7 +572,7 @@ export class Chart extends BasicElement {
    * @param updateMode Additional configuration for control an animation in the update process.
    * @returns {void}
    */
-  public updateChart (updateMode: UpdateMode): void {
+  public updateChart (updateMode?: UpdateMode): void {
     if (!this.chart || !this.config) {
       return;
     }
