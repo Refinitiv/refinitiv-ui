c0abb544dede646d34d092f5f1aa60bdd7a92d37
Nantawat Poothong <102957966+Nantawat-Poothong@users.noreply.github.com>
2023-07-25 17:22:17 +0700

    feat(tooltip): add data-tooltip alias for tooltip attribute (#858)

--- packages/elements/src/tooltip/helpers/renderer.ts
+++ packages/elements/src/tooltip/helpers/renderer.ts
@@ -1,13 +1,13 @@
 import type { TooltipRenderer } from './types';
 
 /**
- * A default renderer that renders `tooltip` attribute
+ * A default renderer that renders `tooltip` or `data-tooltip` attributes
  * @param target Target to check
  * @returns tooltip or null or undefined
  */
 const tooltipRenderer: TooltipRenderer = (target: HTMLElement) => {
-  if (target.hasAttribute('tooltip')) {
-    return target.getAttribute('tooltip');
+  if (target.hasAttribute('tooltip') || target.hasAttribute('data-tooltip')) {
+    return target.getAttribute('tooltip') || target.getAttribute('data-tooltip');
   }
 };
 
