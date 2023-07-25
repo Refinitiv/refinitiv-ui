3fee55852d8797f5208225cd9a0a1baf278878bf
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-07-25 16:48:03 +0700

    chore: configure eslint for unit test

--- packages/configurations/typescript.eslint-config.js
+++ packages/configurations/typescript.eslint-config.js
@@ -6,7 +6,6 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint', 'eslint-plugin-import'],
       extends: [
-        './eslint-config.js',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking'
@@ -23,6 +22,15 @@ module.exports = {
             requireParamType: false
           }
         ]
+      }
+    },
+    {
+      files: ['**/*test.js'],
+      plugins: ['mocha'],
+      extends: ['plugin:mocha/recommended'],
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2022
       }
     }
   ]
