39ebd1141f96f64973ec4e3e407c9af7804d79a0
Nantawat Poothong <102957966+Nantawat-Poothong@users.noreply.github.com>
2023-08-16 13:15:35 +0700

    test: refactor unit test (#898)

--- packages/core/__test__/errors/DuplicateStyleError.test.js
+++ packages/core/__test__/errors/DuplicateStyleError.test.js
@@ -2,6 +2,17 @@ import { expect } from '@refinitiv-ui/test-helpers';
 
 import { DuplicateStyleError } from '../../lib/errors/DuplicateStyleError.js';
 
+const duplicateStyleErrorMessage = `Only one theme file can be loaded per element
+
+[test error] has already been defined.
+
+Potential causes:
+1. You are trying to load a multiple variants of a theme
+2. You have loaded multiple or duplicate themes in your application bundle
+
+https://ui.refinitiv.com/kb/duplicate-styles
+`;
+
 describe('TestDuplicateStyleError', function () {
   it('Test generate simple message', function () {
     try {
@@ -9,7 +20,7 @@ describe('TestDuplicateStyleError', function () {
     } catch (error) {
       expect(error).instanceOf(DuplicateStyleError, 'Not a DuplicateStyleError instance');
       expect(error.message).to.include('test error', 'Wrong error message');
-      expect(error.message).to.equalSnapshot();
+      expect(error.message).to.equal(duplicateStyleErrorMessage);
     }
   });
 });
