05cc5156200f4293d31b52ea84e6facce49567b9
nantawat.poothong <nantawat.poothong@lseg.com>
2023-08-16 10:59:43 +0700

    test(slider): add number-field input event test

--- packages/elements/src/slider/__test__/slider.event.test.js
+++ packages/elements/src/slider/__test__/slider.event.test.js
@@ -7,6 +7,7 @@ import { calculateValue, tabSliderPosition } from './utils.js';
 
 const isDragging = (el) => el.dragging;
 const getSliderTrackElement = (el) => el.sliderRef.value;
+const getNumberField = (el, name) => el.shadowRoot.querySelector(`ef-number-field[name=${name}]`);
 
 describe('slider/Events', function () {
   let el;
@@ -1397,5 +1398,70 @@ describe('slider/Events', function () {
     // Check call fire event
     expect(callCountValue).to.equal(1);
     expect(inputToValue).to.equal(calculateValue(el, dragPosition80).toString());
+  });
+
+  it('Should fires input event when input value from number-field', async function () {
+    el.showInputField = '';
+    await elementUpdated(el);
+
+    const inputEvent = 'input';
+    let callCountValue = 0;
+    let inputValue = 0;
+    el.addEventListener(inputEvent, (e) => {
+      callCountValue += 1;
+      inputValue = e.detail.value;
+    });
+    const input = getNumberField(el, 'value');
+    input.value = '40';
+    setTimeout(() => input.dispatchEvent(new Event('input')));
+    await oneEvent(el, inputEvent);
+
+    // Check call fire event
+    expect(callCountValue).to.equal(1);
+    expect(inputValue).to.equal('40'.toString());
+  });
+
+  it('Should fires from-input event when input value from `from` number-field', async function () {
+    el.showInputField = '';
+    el.range = true;
+    await elementUpdated(el);
+
+    const inputFromEvent = 'from-input';
+    let callCountValue = 0;
+    let inputFromValue = 0;
+    el.addEventListener(inputFromEvent, (e) => {
+      callCountValue += 1;
+      inputFromValue = e.detail.value;
+    });
+    const input = getNumberField(el, 'from');
+    input.value = '40';
+    setTimeout(() => input.dispatchEvent(new Event('input')));
+    await oneEvent(el, inputFromEvent);
+
+    // Check call fire event
+    expect(callCountValue).to.equal(1);
+    expect(inputFromValue).to.equal('40'.toString());
+  });
+
+  it('Should fires to-input event when input value from `to` number-field', async function () {
+    el.showInputField = '';
+    el.range = true;
+    await elementUpdated(el);
+
+    const inputToEvent = 'to-input';
+    let callCountValue = 0;
+    let inputToValue = 0;
+    el.addEventListener(inputToEvent, (e) => {
+      callCountValue += 1;
+      inputToValue = e.detail.value;
+    });
+    const input = getNumberField(el, 'to');
+    input.value = '40';
+    setTimeout(() => input.dispatchEvent(new Event('input')));
+    await oneEvent(el, inputToEvent);
+
+    // Check call fire event
+    expect(callCountValue).to.equal(1);
+    expect(inputToValue).to.equal('40'.toString());
   });
 });
