8a213d1eba71b5e9c7e15f21a78a9b2f9006c461
nantawat.poothong <nantawat.poothong@lseg.com>
2023-08-18 15:28:58 +0700

    fix(slider): add value-changed listener for slider input event

--- packages/elements/src/slider/index.ts
+++ packages/elements/src/slider/index.ts
@@ -1396,6 +1396,7 @@ export class Slider extends ControlElement {
         @blur=${this.onNumberFieldBlur}
         @keydown=${this.onNumberFieldKeyDown}
         @input=${this.onNumberFieldInput}
+        @value-changed=${this.onNumberFieldInput}
         part="input"
         name="${name}"
         no-spinner
