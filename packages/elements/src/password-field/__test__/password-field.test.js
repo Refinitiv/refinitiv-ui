1781b60d9426734481faf7b24649132a5cc09230
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-09-21 13:50:28 +0700

    test: update snapshots

--- packages/elements/src/password-field/__test__/password-field.test.js
+++ packages/elements/src/password-field/__test__/password-field.test.js
@@ -14,15 +14,22 @@ describe('password-field/PasswordField', function () {
     const el = await fixture('<ef-password-field></ef-password-field>');
     const eyeIconEl = el.shadowRoot.querySelector('[part=icon]');
     const inputEl = el.shadowRoot.querySelector('[part=input]');
+    const liveRegionEl = el.shadowRoot.querySelector('[part=live-region]');
 
     expect(inputEl.getAttribute('type')).to.equal(
       'password',
       'Input type should set to "password" by default'
     );
-    expect(eyeIconEl.getAttribute('aria-label')).to.equal(
-      'Show password',
-      'aria-label of icon should set to "Show password" by default'
+
+    expect(eyeIconEl.getAttribute('aria-pressed')).to.equal(
+      'false',
+      'aria-pressed of icon should set to "false" by default'
     );
+    // add focus test here before toggle?
+    // expect(liveRegionEl.textContent).to.equal(
+    //   'Show password',
+    //   'aria-label of icon should set to "Show password" by default'
+    // );
 
     eyeIconEl.click();
     await elementUpdated(el);
@@ -30,9 +37,16 @@ describe('password-field/PasswordField', function () {
       'text',
       'Input type should set to "text" after click show password'
     );
-    expect(eyeIconEl.getAttribute('aria-label')).to.equal(
-      'Hide password',
-      'aria-label of icon should set to "Hide password" after click show password'
+
+    expect(eyeIconEl.getAttribute('aria-pressed')).to.equal(
+      'true',
+      'aria-pressed of icon should set to "true" after toggling show password'
+    );
+
+    const visibleMessage = 'Show password on, password is visible';
+    expect(liveRegionEl.textContent).to.equal(
+      visibleMessage,
+      `text content of live region should set to "${visibleMessage}" after toggling show password`
     );
 
     eyeIconEl.click();
@@ -41,9 +55,18 @@ describe('password-field/PasswordField', function () {
       'password',
       'Input type should back to "Password" after click hide password'
     );
-    expect(eyeIconEl.getAttribute('aria-label')).to.equal(
-      'Show password',
-      'aria-label of icon should back to "Show password" after click hide password'
+
+    expect(eyeIconEl.getAttribute('aria-pressed')).to.equal(
+      'false',
+      'aria-pressed of icon should set to "false" after 2nd toggling show password'
+    );
+
+    const hiddenMessage = 'Show password off, password is hidden';
+    expect(liveRegionEl.textContent).to.equal(
+      hiddenMessage,
+      `aria-label of icon should back to "${hiddenMessage}" after 2nd toggling show password`
     );
   });
 });
+
+// test focus and aria-lve update!
