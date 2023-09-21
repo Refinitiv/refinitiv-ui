c76cf9b1677d4abd668b00ef0a259c2a688fd096
nantawat.poothong <nantawat.poothong@lseg.com>
2023-09-21 18:12:15 +0700

    fix(tree): add parent label can't expand/collapse in stateless mode

--- packages/elements/src/tree/elements/tree.ts
+++ packages/elements/src/tree/elements/tree.ts
@@ -117,6 +117,9 @@ export class Tree<T extends TreeDataItem = TreeDataItem> extends List<T> {
   public override selectItem(item: T): boolean {
     // Stateless tree
     if (this.stateless) {
+      if (this.manager.isItemParent(item)) {
+        this.toggleExpandedState(item);
+      }
       return false;
     }
     // Multiple selection
