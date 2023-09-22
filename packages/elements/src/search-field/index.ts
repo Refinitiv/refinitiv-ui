884f011691a1a76e31cfa93e09a8f0ba515a35df
Sarin-Udompanish <86759822+Sarin-Udompanish@users.noreply.github.com>
2023-09-22 14:01:18 +0700

    docs: update description of reportValidity method
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>

--- packages/elements/src/search-field/index.ts
+++ packages/elements/src/search-field/index.ts
@@ -69,8 +69,8 @@ export class SearchField extends TextField {
   }
 
   /**
-   * Returns true if an input element contains valid data
-   * @returns true if input is valid
+   * Returns `true` if the element input is valid; otherwise, returns `false`.
+   * @returns element input validity
    */
   public override checkValidity(): boolean {
     return super.checkValidity();
