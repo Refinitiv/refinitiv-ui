5eb3c256fd2b0c585f40f4872c85e6c50050ded8
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-09-27 16:53:39 +0700

    refactor(demo-block): fix innerHTML vulnerability

--- packages/demo-block/src/demo-block.ts
+++ packages/demo-block/src/demo-block.ts
@@ -101,7 +101,7 @@ themeLabel.style.top = '10px';
 themeLabel.style.padding = '2px 4px';
 themeLabel.style.fontWeight = '600';
 themeLabel.style.textTransform = 'uppercase';
-themeLabel.innerHTML = removePrefixName(currentTheme);
+themeLabel.textContent = removePrefixName(currentTheme);
 
 const versionTag = document.createElement('span');
 versionTag.innerHTML = 'V6';
