a3a37d71d6a5e90809142aaba9fa7516e471d733
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-10-06 17:12:13 +0700

    refactor: update Japanese & German phrasebook for password-field

--- packages/phrasebook/src/locale/de/password-field.ts
+++ packages/phrasebook/src/locale/de/password-field.ts
@@ -2,8 +2,8 @@ import { Phrasebook } from '../../translation.js';
 
 const translations = {
   SHOW_PASSWORD: 'Kennwort anzeigen',
-  SHOW_PASSWORD_ON: 'Kennwort anzeigen am, Kennwort ist sichtbar', // machine translated
-  SHOW_PASSWORD_OFF: 'Kennwort anzeigen aus, Kennwort ist ausgeblendet' // machine translated
+  SHOW_PASSWORD_ON: 'Passwort anzeigen ein, Passwort wird angezeigt',
+  SHOW_PASSWORD_OFF: 'Passwort anzeigen aus, Passwort wird ausgeblendet'
 };
 
 Phrasebook.define('de', 'ef-password-field', translations);
--- packages/phrasebook/src/locale/ja/password-field.ts
+++ packages/phrasebook/src/locale/ja/password-field.ts
@@ -2,8 +2,8 @@ import { Phrasebook } from '../../translation.js';
 
 const translations = {
   SHOW_PASSWORD: 'パスワード表示',
-  SHOW_PASSWORD_ON: 'パスワード表示オン、 パスワードが表れています',
-  SHOW_PASSWORD_OFF: 'パスワード表示オッフ、 パスワードが隠れています'
+  SHOW_PASSWORD_ON: 'パスワード表示オン、パスワードが表示されています',
+  SHOW_PASSWORD_OFF: 'パスワード表示オフ、パスワードは表示されていません'
 };
 
 Phrasebook.define('ja', 'ef-password-field', translations);
