1a1fe749d3a35c0834e7261bcd9a37306edbd093
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2023-10-09 15:10:57 +0700

    refactor: update simplified & traditional Chinese translation

--- packages/phrasebook/src/locale/zh-hant/password-field.ts
+++ packages/phrasebook/src/locale/zh-hant/password-field.ts
@@ -2,8 +2,8 @@ import { Phrasebook } from '../../translation.js';
 
 const translations = {
   SHOW_PASSWORD: '顯示密碼',
-  SHOW_PASSWORD_ON: '顯示密碼開啟，密碼可見', // machine translated
-  SHOW_PASSWORD_OFF: '顯示密碼關閉，密碼被隱藏' // machine translated
+  SHOW_PASSWORD_ON: '密碼顯示開啟，密碼可見',
+  SHOW_PASSWORD_OFF: '密碼顯示關閉，密碼被隱藏'
 };
 
 Phrasebook.define('zh-Hant', 'ef-password-field', translations);
--- packages/phrasebook/src/locale/zh/password-field.ts
+++ packages/phrasebook/src/locale/zh/password-field.ts
@@ -2,8 +2,8 @@ import { Phrasebook } from '../../translation.js';
 
 const translations = {
   SHOW_PASSWORD: '显示密码',
-  SHOW_PASSWORD_ON: '显示密码开启，密码可见', // machine translated
-  SHOW_PASSWORD_OFF: '显示密码关闭，密码被隐藏' // machine translated
+  SHOW_PASSWORD_ON: '密码显示开启，密码可见',
+  SHOW_PASSWORD_OFF: '密码显示关闭，密码被隐藏'
 };
 
 Phrasebook.define('zh', 'ef-password-field', translations);
