61d535136ee6e191cf49b7d6b55e81f4dd6e5f06
bualoy-napat <napat.bualoy2@lseg.com>
2023-10-27 17:33:05 +0700

    test(datetime-picker): update snapshot for custom cell slot

--- packages/elements/src/datetime-picker/__test__/datetime-picker.snapshot.test.js
+++ packages/elements/src/datetime-picker/__test__/datetime-picker.snapshot.test.js
@@ -2,7 +2,7 @@
 import '@refinitiv-ui/elements/datetime-picker';
 
 import '@refinitiv-ui/elemental-theme/light/ef-datetime-picker';
-import { expect, fixture, nextFrame } from '@refinitiv-ui/test-helpers';
+import { elementUpdated, expect, fixture, nextFrame } from '@refinitiv-ui/test-helpers';
 
 import { snapshotIgnore } from './utils.js';
 
@@ -57,6 +57,47 @@ describe('datetime-picker/DOMStructure', function () {
       const el = await fixture(
         '<ef-datetime-picker lang="en-gb" view="2020-04" timepicker range opened></ef-datetime-picker>'
       );
+      await nextFrame();
+      await nextFrame();
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+    });
+    it('DOM structure is correct when add custom cell slot of calendar without prefix', async function () {
+      const el = await fixture(
+        '<ef-datetime-picker lang="en-gb" view="2020-04">' +
+          '<div slot="2020-04-01">slotted</div>' +
+          '</ef-datetime-picker>'
+      );
+      el.opened = true;
+
+      await elementUpdated(el);
+      await nextFrame();
+      await nextFrame();
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+    });
+    it('DOM structure is correct when add custom cell slot of calendar with prefix', async function () {
+      const el = await fixture(
+        '<ef-datetime-picker lang="en-gb" view="2020-04" duplex>' +
+          '<div slot="from-2020-04-01">slotted</div>' +
+          '<div slot="to-2020-05-01">slotted</div>' +
+          '</ef-datetime-picker>'
+      );
+      el.opened = true;
+
+      await elementUpdated(el);
+      await nextFrame();
+      await nextFrame();
+      await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
+    });
+    it('DOM structure is correct when add custom cell slot of calendar while opened', async function () {
+      const el = await fixture(
+        '<ef-datetime-picker lang="en-gb" view="2020-04" duplex opened>' +
+          '<div slot="from-2020-04-01">slotted</div>' +
+          '<div slot="to-2020-05-01">slotted</div>' +
+          '</ef-datetime-picker>'
+      );
+      el.updateCalendar();
+
+      await elementUpdated(el);
       await nextFrame();
       await nextFrame();
       await expect(el).shadowDom.to.equalSnapshot(snapshotIgnore);
