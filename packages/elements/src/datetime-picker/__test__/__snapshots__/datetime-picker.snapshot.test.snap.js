ba934de651511c60cb8d78906fd779e12d0a347d
bualoy-napat <napat.bualoy2@lseg.com>
2023-11-23 15:58:39 +0700

    test(datetime-picker): update snapshot and test case

--- packages/elements/src/datetime-picker/__test__/__snapshots__/datetime-picker.snapshot.test.snap.js
+++ packages/elements/src/datetime-picker/__test__/__snapshots__/datetime-picker.snapshot.test.snap.js
@@ -501,83 +501,6 @@ snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct w
 `;
 /* end snapshot datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar without prefix */
 
-snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar prefix"] = 
-`<div part="input-wrapper">
-  <ef-text-field
-    id="input"
-    part="input"
-    tabindex="0"
-    transparent=""
-  >
-  </ef-text-field>
-</div>
-<ef-icon
-  icon="calendar"
-  part="icon"
->
-</ef-icon>
-<ef-overlay-viewport>
-</ef-overlay-viewport>
-<ef-overlay
-  no-autofocus=""
-  no-cancel-on-esc-key=""
-  opened=""
-  part="list"
-  tabindex="0"
-  with-shadow=""
->
-  <div>
-    <slot name="header">
-    </slot>
-  </div>
-  <div part="body">
-    <div>
-      <slot name="left">
-      </slot>
-    </div>
-    <div part="selectors-wrapper">
-      <div part="calendar-wrapper">
-        <ef-calendar
-          id="calendar"
-          lang="en-gb"
-          part="calendar"
-          role="group"
-          tabindex="0"
-        >
-          <slot
-            name="from-2020-04-01"
-            slot="2020-04-01"
-          >
-          </slot>
-        </ef-calendar>
-        <ef-calendar
-          id="calendar-to"
-          lang="en-gb"
-          part="calendar"
-          role="group"
-          tabindex="0"
-        >
-          <slot
-            name="to-2020-05-01"
-            slot="2020-05-01"
-          >
-          </slot>
-        </ef-calendar>
-      </div>
-    </div>
-    <div>
-      <slot name="right">
-      </slot>
-    </div>
-  </div>
-  <div>
-    <slot name="footer">
-    </slot>
-  </div>
-</ef-overlay>
-`;
-/* end snapshot datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar prefix */
-
 snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar with prefix"] = 
 `<div part="input-wrapper">
   <ef-text-field
@@ -655,7 +578,7 @@ snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct w
 `;
 /* end snapshot datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar with prefix */
 
-snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar while opened"] = 
+snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar while overlay is opened"] = 
 `<div part="input-wrapper">
   <ef-text-field
     id="input"
@@ -730,5 +653,69 @@ snapshots["datetime-picker/DOMStructure DOM Structure DOM structure is correct w
   </div>
 </ef-overlay>
 `;
-/* end snapshot datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar while opened */
+/* end snapshot datetime-picker/DOMStructure DOM Structure DOM structure is correct when add custom cell slot of calendar while overlay is opened */
+
+snapshots["datetime-picker/DOMStructure DOM Structure DOM structure should not contain added custom cell slot when overlay is closed"] = 
+`<div part="input-wrapper">
+  <ef-text-field
+    id="input"
+    part="input"
+    tabindex="0"
+    transparent=""
+  >
+  </ef-text-field>
+</div>
+<ef-icon
+  icon="calendar"
+  part="icon"
+>
+</ef-icon>
+<ef-overlay
+  no-autofocus=""
+  no-cancel-on-esc-key=""
+  part="list"
+  tabindex="0"
+  with-shadow=""
+>
+  <div>
+    <slot name="header">
+    </slot>
+  </div>
+  <div part="body">
+    <div>
+      <slot name="left">
+      </slot>
+    </div>
+    <div part="selectors-wrapper">
+      <div part="calendar-wrapper">
+        <ef-calendar
+          id="calendar"
+          lang="en-gb"
+          part="calendar"
+          role="group"
+          tabindex="0"
+        >
+        </ef-calendar>
+        <ef-calendar
+          id="calendar-to"
+          lang="en-gb"
+          part="calendar"
+          role="group"
+          tabindex="0"
+        >
+        </ef-calendar>
+      </div>
+    </div>
+    <div>
+      <slot name="right">
+      </slot>
+    </div>
+  </div>
+  <div>
+    <slot name="footer">
+    </slot>
+  </div>
+</ef-overlay>
+`;
+/* end snapshot datetime-picker/DOMStructure DOM Structure DOM structure should not contain added custom cell slot when overlay is closed */
 
