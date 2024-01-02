1604307a877e03931123b1da30ce818f34407fb8
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2024-01-02 14:24:19 +0700

    refactor: replace RegExp with SVGO for inline SVG optimization

--- packages/theme-compiler/src/less-plugin-inline-svg/encoders.js
+++ packages/theme-compiler/src/less-plugin-inline-svg/encoders.js
@@ -1,13 +1,11 @@
-const RE2 = require('re2');
+const { optimize } = require('svgo');
 
 const encodeHtmlToBase64 = (input) => Buffer.from(input).toString('base64');
 const encodeHtmlEntities = (input) => encodeURIComponent(input);
-// replace native RegExp with RE2 to prevent ReDos
-// https://owasp.org/www-community/attacks/Regular_expression_Denial_of_Service_-_ReDoS
-const optimizeSvg = (input) => input.replace(new RE2(/\s*\n\s*/g), '');
+const optimizeSvg = (input) => optimize(input).data;
 
 const wrapAndEncodeSvgToBase64 = (svgCode) =>
-  `url("data:image/svg+xml;base64,${encodeHtmlToBase64(svgCode)}")`;
+  `url("data:image/svg+xml;base64,${encodeHtmlToBase64(optimizeSvg(svgCode))}")`;
 
 const wrapSvg = (svgCode) => `url('data:image/svg+xml;charset=UTF-8,${optimizeSvg(svgCode)}')`;
 
