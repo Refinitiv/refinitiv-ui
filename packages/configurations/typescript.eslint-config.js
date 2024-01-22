109279e0ba315af14bf3fc620382eb3e4022beba
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2024-01-22 18:01:50 +0700

    refactor: add clarification comment

--- packages/configurations/typescript.eslint-config.js
+++ packages/configurations/typescript.eslint-config.js
@@ -29,12 +29,12 @@ module.exports = {
       files: ['*test.js'],
       plugins: ['mocha'],
       extends: ['plugin:mocha/recommended'],
+      rules: {
+        'mocha/max-top-level-suites': 0
+      },
       parserOptions: {
         sourceType: 'module',
         ecmaVersion: 2022
-      },
-      rules: {
-        'mocha/max-top-level-suites': 0
       }
     }
   ]
