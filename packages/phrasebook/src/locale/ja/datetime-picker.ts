a2fad801e99f6d8271e99e95822964b7987eec17
kosol komenek <kosol.komenek@refinitiv.com>
2024-02-09 17:52:13 +0700

    fix(datetime-picker): cannot be used with label with aria-label, aria-labelledby and label (#1079)
    * fix(datetime-picker): update change base class from ControlElement to FormFieldElement
    
    * fix(datetime-picker): update accessibility
    
    * fix(datetime-picker): update demo page back to en-GB
    
    * fix(datetime-picker): update document
    
    * fix(datetime-picker): update label
    
    * fix(datetime-picker): fix default to download from en
    
    * fix(datetime-picker): update import translations
    
    * fix(datetime-picker): update feedback
    
    * Update packages/elements/src/datetime-picker/locales.ts
    
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>
    
    * Update packages/elements/src/datetime-picker/index.ts
    
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>
    
    * Update packages/elements/src/datetime-picker/constants.ts
    
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>
    
    * docs(datetime-picker): add double qoute and id
    
    * refactor(datetime-picker): add jsdoc and move variable constants to one point
    
    * docs(datetime-picker): add label example
    
    * refactor(datetime-picker): remove ifDefined
    
    * test(datetime-picker): add test case to check accessibility
    
    * docs(datetime-picker): update label
    
    * docs(datetime-picker): refactor label
    
    * docs(datetime-picker): refactor label position
    
    * docs(datetime-picker): refactor label
    
    * docs: add phrasebook for datetime-picker
    
    * docs(datetime-picker): update label name in a11y section
    
    * Add interface snippet for Data property interface section (#1097)
    
    * docs: add data interface specification
    
    * docs(datetime-picker): add indent and remove unnecessary tag section
    
    * refactor(datetime-picker): replace string constant variable
    
    * docs(datetime-picker): refactor label in custom section
    
    ---------
    
    Co-authored-by: Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>
    Co-authored-by: Napat Bualoy <napat.bualoy@lseg.com>
    Co-authored-by: Domrongpon Tanpaibul <81604092+dtanp-rft@users.noreply.github.com>

---
+++ packages/phrasebook/src/locale/ja/datetime-picker.ts
@@ -0,0 +1,10 @@
+import { Phrasebook } from '../../translation.js';
+
+const translations = {
+  FROM: '開始',
+  TO: '終了'
+};
+
+Phrasebook.define('ja', 'ef-datetime-picker', translations);
+
+export default translations;
