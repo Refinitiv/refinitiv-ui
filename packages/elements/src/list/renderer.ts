564c312279fe2e728f5ed1c400ae92df8c2225ae
Theeraphat-Sorasetsakul <86758473+Theeraphat-Sorasetsakul@users.noreply.github.com>
2024-03-07 10:42:41 +0700

    Update packages/elements/src/list/renderer.ts
    Co-authored-by: wattachai <117723407+wattachai-lseg@users.noreply.github.com>

--- packages/elements/src/list/renderer.ts
+++ packages/elements/src/list/renderer.ts
@@ -3,6 +3,6 @@ import { ExtensibleFunction } from './extensible-function.js';
 /**
  * Renderer base class.
  * Used for creating renderers to render data items.
- * @deprecate Don't use this! It causes uncaught EvalError when using script-src 'self' CSP.
+ * @deprecate Renderer is deprecated.
  */
 export abstract class Renderer extends ExtensibleFunction {}
