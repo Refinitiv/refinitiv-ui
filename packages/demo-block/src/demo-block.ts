06f3811e49ef84f60ffe88840031d150a70d3a12
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2024-03-14 02:04:37 +0700

    fix(tooltip): handle body removal

--- packages/demo-block/src/demo-block.ts
+++ packages/demo-block/src/demo-block.ts
@@ -78,9 +78,9 @@ themeLabel.style.fontWeight = '600';
 themeLabel.style.textTransform = 'uppercase';
 themeLabel.textContent = removePrefixName(currentTheme);
 
-document.body.appendChild(nextBtn);
-document.body.appendChild(prevBtn);
-document.body.appendChild(themeLabel);
+document.body?.appendChild(nextBtn);
+document.body?.appendChild(prevBtn);
+document.body?.appendChild(themeLabel);
 
 @customElement('demo-block', { theme: false })
 export class DemoBlock extends BasicElement {
