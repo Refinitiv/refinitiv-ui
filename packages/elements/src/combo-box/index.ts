c2b648ed0b7c63c0bc4eab557af8c6b492fd5363
Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>
2024-03-15 19:46:28 +0700

    Update packages/elements/src/combo-box/index.ts
    Co-authored-by: Domrongpon Tanpaibul <81604092+dtanp-rft@users.noreply.github.com>

--- packages/elements/src/combo-box/index.ts
+++ packages/elements/src/combo-box/index.ts
@@ -280,7 +280,7 @@ export class ComboBox<T extends DataItem = ItemData> extends FormFieldElement {
       const oldValues = this.values.slice();
 
       // Create comparison strings to check for differences.
-      // This sort function didn't support i18n at this sort, and
+      // i18n not required at this sort, and
       // we just sort values to create signature values for comparison.
       const newComparison = newValues.sort().toString();
       const oldComparison = oldValues.sort().toString();
