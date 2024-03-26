6c38542565352eea7f02cdcfe272b06a5fde750e
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2024-03-26 10:20:06 +0700

    refactor: revise TODO comment

--- packages/elements/src/tree/elements/tree.ts
+++ packages/elements/src/tree/elements/tree.ts
@@ -299,8 +299,7 @@ export class Tree<T extends TreeDataItem = TreeDataItem> extends List<T> {
       const filter = this.filter;
       const items = this.queryItems((item): boolean => {
         // Do not filter hidden items
-        // TODO: incompatible with `hidden` update via TreeManager/TreeNode
-        // apply to Tree & Combo-Box component
+        // TODO: incompatible with `hidden` update via TreeManager/TreeNode for Tree & Combo-Box component
         if (item.hidden) {
           return false;
         }
--- packages/utils/src/collection/collection-composer.ts
+++ packages/utils/src/collection/collection-composer.ts
@@ -245,8 +245,7 @@ export class CollectionComposer<T extends CollectionItem = CollectionItem> exten
    * @return {void}
    */
   public addItem(item: T, parent: T, index: number): void;
-  // TODO: incompatible with isItemParent()
-  // There is no parent's `items` update here but isItemParent() relies on it
+  // TODO: incompatible with isItemParent() as there is no parent's `items` update required by isItemParent().
   public addItem(item: T, parentOrIndex?: T | number, index: number = parentOrIndex as number): void {
     const parent = parentOrIndex && typeof parentOrIndex !== 'number' ? parentOrIndex : null;
     if (parent && this.isInvalidItem(parent)) {
@@ -487,8 +486,7 @@ export class CollectionComposer<T extends CollectionItem = CollectionItem> exten
     let index = this.items.indexOf(item);
     while (index > 0) {
       const next = this.items[(index -= 1)];
-      // TODO: incompatible with addItem()
-      // addItem() doesn't update `items` of a parent
+      // TODO: incompatible with addItem() as addItem() doesn't update parent's `items`.
       if (next.items?.includes(item)) {
         return next;
       }
