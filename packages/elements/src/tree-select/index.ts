ac4301045819ef4eb5598bfdd2e21837397e6341
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2024-05-24 20:12:43 +0700

    docs(tree-select): workaround overflow table issue in API reference

--- packages/elements/src/tree-select/index.ts
+++ packages/elements/src/tree-select/index.ts
@@ -171,8 +171,11 @@ export class TreeSelect extends ComboBox<TreeSelectDataItem> {
   protected override composer: CollectionComposer<TreeSelectDataItem> = new CollectionComposer([]);
 
   protected _treeManager: TreeManager<TreeSelectDataItem> = new TreeManager(this.composer);
+
+  // add a space in front of angle bracket for line break opportunity in EF docs with @type
   /**
    * Tree manager used for item manipulation
+   * @type {TreeManager <TreeSelectDataItem>}
    */
   public get treeManager(): TreeManager<TreeSelectDataItem> {
     return this._treeManager;
