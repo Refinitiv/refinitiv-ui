3776ed793f7fe6a86a9b2996f832730baa439431
Wattachai Kanawitoon <117723407+wattachai-lseg@users.noreply.github.com>
2024-05-30 15:58:27 +0700

    test(combo-box): add custom filter unit test

--- packages/elements/src/combo-box/__test__/__snapshots__/combo-box.filter.test.snap.js
+++ packages/elements/src/combo-box/__test__/__snapshots__/combo-box.filter.test.snap.js
@@ -125,3 +125,62 @@ snapshots["combo-box/Filter Can Filter Data Default filter filters data: changed
 `;
 /* end snapshot combo-box/Filter Can Filter Data Default filter filters data: changed */
 
+snapshots["combo-box/Filter Can Filter Data Should be able to use custom filter function"] = 
+`<div part="input-wrapper">
+  <input
+    aria-activedescendant="AX"
+    aria-autocomplete="list"
+    aria-expanded="true"
+    aria-haspopup="listbox"
+    aria-owns="internal-list"
+    autocomplete="off"
+    part="input"
+    role="combobox"
+    type="text"
+  >
+  <div
+    id="toggle-button"
+    part="button button-toggle"
+  >
+    <ef-icon
+      icon="down"
+      part="icon icon-toggle"
+    >
+    </ef-icon>
+  </div>
+</div>
+<ef-overlay-viewport>
+</ef-overlay-viewport>
+<ef-overlay
+  no-autofocus=""
+  no-focus-management=""
+  no-overlap=""
+  opened=""
+  part="list"
+  tabindex="-1"
+  with-shadow=""
+>
+  <ef-list
+    aria-multiselectable="false"
+    id="internal-list"
+    role="listbox"
+    tabindex=""
+  >
+    <ef-list-item
+      aria-selected="false"
+      role="presentation"
+      type="header"
+    >
+    </ef-list-item>
+    <ef-list-item
+      aria-selected="false"
+      highlighted=""
+      id="AX"
+      role="option"
+    >
+    </ef-list-item>
+  </ef-list>
+</ef-overlay>
+`;
+/* end snapshot combo-box/Filter Can Filter Data Should be able to use custom filter function */
+
