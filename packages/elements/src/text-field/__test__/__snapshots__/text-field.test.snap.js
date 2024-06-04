c0d7ff6988c0aa9d4e7ea6a09bcfcbe4ab02ae21
Theeraphat Sorasetsakul <Theeraphat.Sorasetsakul@refinitiv.com>
2024-06-04 16:29:48 +0700

    test: fix test

--- packages/elements/src/text-field/__test__/__snapshots__/text-field.test.snap.js
+++ packages/elements/src/text-field/__test__/__snapshots__/text-field.test.snap.js
@@ -1,34 +1,35 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["text-field/TextField DOM structure and properties are correct"] =
-`<input  aria-invalid="true"  autocomplete="off"  maxlength="10"  minlength="5"  part="input"  pattern="[a-z]"  placeholder="Placeholder"  type="text"><ef-icon  icon="menu"  part="icon"></ef-icon>`;
-/* end snapshot text-field/TextField DOM structure and properties are correct */
-
-snapshots["text-field/TextField Default DOM structure and properties are correct"] =
-`<input  autocomplete="off"  part="input"  type="text">`;
-/* end snapshot text-field/TextField Default DOM structure and properties are correct */
-
-snapshots["text-field/DOM structure with clears is correct"] =
+snapshots["text-field/TextField Default DOM structure and properties are correct"] = 
 `<input
   autocomplete="off"
   part="input"
   type="text"
 >
-<div
-  id="clears-button"
-  part="button button-clear"
->
-  <ef-icon
-    icon="cross"
-    part="icon icon-clear"
-  >
-  </ef-icon>
-</div>
 `;
-/* end snapshot text-field/TextField Should have correct "clears" Shadow DOM structure */
+/* end snapshot text-field/TextField Default DOM structure and properties are correct */
 
-snapshots["text-field/TextField DOM structure with clears is correct"] =
+snapshots["text-field/TextField DOM structure and properties are correct"] = 
+`<input
+  aria-invalid="true"
+  autocomplete="off"
+  maxlength="10"
+  minlength="5"
+  part="input"
+  pattern="[a-z]"
+  placeholder="Placeholder"
+  type="text"
+>
+<ef-icon
+  icon="menu"
+  part="icon"
+>
+</ef-icon>
+`;
+/* end snapshot text-field/TextField DOM structure and properties are correct */
+
+snapshots["text-field/TextField DOM structure with clears is correct"] = 
 `<input
   autocomplete="off"
   part="input"
