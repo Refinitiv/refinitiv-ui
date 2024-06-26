081f319c248bbe56130e67d8ac54090db45f2a9e
Theeraphat-Sorasetsakul <86758473+Theeraphat-Sorasetsakul@users.noreply.github.com>
2024-06-26 13:12:51 +0700

    Update packages/core/src/elements/FormFieldElement.ts

--- packages/core/src/elements/FormFieldElement.ts
+++ packages/core/src/elements/FormFieldElement.ts
@@ -414,6 +414,7 @@ export abstract class FormFieldElement extends ControlElement {
     if (this.hasClear) {
       return html`
         <div
+          aria-hidden="true"
           ${ref(this.clearsButtonRef)}
           id="clears-button"
           part="button button-clear"
