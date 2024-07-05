9bd8613779d31bac6db2d336ac491ab9c2eaa7c3
prima-lseg <prima.sirinapapant@lseg.com>
2024-07-05 14:37:14 +0700

    fix(card): expand body instead of header and footer (#1191)
    * fix(card): expand body instead of header and footer
    
    * Update packages/elements/src/card/index.ts
    
    Co-authored-by: Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>
    
    * Update packages/elements/src/card/index.ts
    
    Co-authored-by: Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Napat Bualoy <91940544+bualoy-napat@users.noreply.github.com>

--- packages/elements/src/card/index.ts
+++ packages/elements/src/card/index.ts
@@ -49,13 +49,26 @@ export class Card extends BasicElement {
         display: flex;
         flex-flow: column nowrap;
       }
+
       [part~='header'] {
         display: flex;
       }
+
       [part~='header-body'] {
         flex: 1;
         min-width: 0px;
       }
+
+      [part~='body'] {
+        overflow: auto;
+        flex: 1;
+      }
+
+      [part~='header'],
+      [part~='footer'] {
+        flex: 0 0 auto;
+      }
+
       [part~='footer']:not([part~='has-content']),
       [part~='header']:not([part~='has-content']) {
         display: none;
