0900aafd96f8eefd5dde072940f8156ce2ed171f
prima.s <prima.sirinapapant@lseg.com>
2024-10-30 17:07:40 +0700

    test(combo-box): import atimeout

--- packages/elements/src/combo-box/__test__/combo-box.interaction.test.js
+++ packages/elements/src/combo-box/__test__/combo-box.interaction.test.js
@@ -1,7 +1,7 @@
 import '@refinitiv-ui/elements/combo-box';
 
 import '@refinitiv-ui/elemental-theme/light/ef-combo-box';
-import { elementUpdated, expect, fixture, nextFrame } from '@refinitiv-ui/test-helpers';
+import { aTimeout, elementUpdated, expect, fixture, nextFrame } from '@refinitiv-ui/test-helpers';
 
 import { dispatchCustomEvent, getData, makeQueryRequest, onFocusEl, openedUpdated } from './utils.js';
 
