0248d06d9f6c704d75896a97166a0fe5e9eaebca
prima.s <prima.sirinapapant@lseg.com>
2024-10-31 11:31:31 +0700

    test(combo-box): add expect output

--- packages/elements/src/combo-box/__test__/combo-box.interaction.test.js
+++ packages/elements/src/combo-box/__test__/combo-box.interaction.test.js
@@ -156,8 +156,8 @@ describe('combo-box/Interaction', function () {
       const focused = el.getAttribute('focused');
       // eslint-disable-next-line no-console
       console.log('focused', focused);
-      // const afItem = el.listEl.querySelector('ef-list-item[highlighted]'); // AF, Afghanistan
-      // expect(afItem.value).to.equal('AF', 'The first non header item should be highlighted');
+      const afItem = el.listEl.querySelector('ef-list-item[highlighted]'); // AF, Afghanistan
+      expect(afItem.value).to.equal('AF', 'The first non header item should be highlighted');
       const item = el.listEl.querySelectorAll('ef-list-item');
       // eslint-disable-next-line no-console
       console.log(item);
